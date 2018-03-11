e84be0da6085c250f6d74a8684d61d0c00321d51
Wei-Sheng Chin <wschin@outlook.com>
2018-03-10 18:36:07 -0800

    Update CoreML type parsing for incorporating info

--- onnxmltools/convert/coreml/_data_types.py
+++ onnxmltools/convert/coreml/_data_types.py
@@ -3,9 +3,10 @@ from ...proto import onnx_proto
 import numpy as np
 
 class DataType(object):
-    def __init__(self, type_name, shape=None):
+    def __init__(self, type_name, shape=None, doc_string=''):
         self.type_name = type_name
         self.shape = shape
+        self.doc_string = doc_string
 
     def __str__(self):
         return '%s' % self.type_name
@@ -15,8 +16,8 @@ class DataType(object):
 
 
 class Int64Type(DataType):
-    def __init__(self):
-        super(Int64Type, self).__init__('INT64', [1, 1])
+    def __init__(self, doc_string=''):
+        super(Int64Type, self).__init__('INT64', [1, 1], doc_string)
 
     def to_onnx_type(self):
         onnx_type = onnx_proto.TypeProto()
@@ -27,8 +28,8 @@ class Int64Type(DataType):
 
 
 class FloatType(DataType):
-    def __init__(self):
-        super(FloatType, self).__init__('FLOAT', [1, 1])
+    def __init__(self, doc_string=''):
+        super(FloatType, self).__init__('FLOAT', [1, 1], doc_string)
 
     def to_onnx_type(self):
         onnx_type = onnx_proto.TypeProto()
@@ -39,8 +40,8 @@ class FloatType(DataType):
 
 
 class StringType(DataType):
-    def __init__(self):
-        super(StringType, self).__init__('STRING', [1, 1])
+    def __init__(self, doc_string=''):
+        super(StringType, self).__init__('STRING', [1, 1], doc_string)
 
     def to_onnx_type(self):
         onnx_type = onnx_proto.TypeProto()
@@ -51,8 +52,8 @@ class StringType(DataType):
 
 
 class TensorType(DataType):
-    def __init__(self, type_name, element_type, shape=[]):
-        super(TensorType, self).__init__(type_name, shape)
+    def __init__(self, type_name, element_type, shape=[], doc_string=''):
+        super(TensorType, self).__init__(type_name, shape, doc_string)
         self.element_type = element_type
 
     def __str__(self):
@@ -60,8 +61,8 @@ class TensorType(DataType):
 
 
 class Int64TensorType(TensorType):
-    def __init__(self, shape=[]):
-        super(Int64TensorType, self).__init__('INT64S', Int64Type(), shape)
+    def __init__(self, shape=[], doc_string=''):
+        super(Int64TensorType, self).__init__('INT64S', Int64Type(), shape, doc_string)
 
     def to_onnx_type(self):
         onnx_type = onnx_proto.TypeProto()
@@ -78,8 +79,8 @@ class Int64TensorType(TensorType):
 
 
 class FloatTensorType(TensorType):
-    def __init__(self, shape=[], color_space=None):
-        super(FloatTensorType, self).__init__('FLOATS', FloatType(), shape)
+    def __init__(self, shape=[], color_space=None, doc_string=''):
+        super(FloatTensorType, self).__init__('FLOATS', FloatType(), shape, doc_string)
         self.color_space = color_space
 
     def to_onnx_type(self):
@@ -97,8 +98,8 @@ class FloatTensorType(TensorType):
 
 
 class StringTensorType(TensorType):
-    def __init__(self, shape=[]):
-        super(StringTensorType, self).__init__('STRINGS', StringType(), shape)
+    def __init__(self, shape=[], doc_string=''):
+        super(StringTensorType, self).__init__('STRINGS', StringType(), shape, doc_string)
 
     def to_onnx_type(self):
         onnx_type = onnx_proto.TypeProto()
@@ -115,11 +116,12 @@ class StringTensorType(TensorType):
 
 
 class DictionaryType(object):
-    def __init__(self, key_type, value_type, max_cardinality=None):
+    def __init__(self, key_type, value_type, max_cardinality=None, doc_string=''):
         self.type_name = 'MAP'
         self.key_type = key_type
         self.value_type = value_type
         self.max_cardinality = max_cardinality  # It is only used to encode length of sparse vector. For other cases, it should be None.
+        self.doc_string = doc_string
 
     def __str__(self):
         return '%s-%s %s' % (str(self.key_type), str(self.value_type), self.type_name)
@@ -135,10 +137,11 @@ class DictionaryType(object):
 
 
 class SequenceType(DataType):
-    def __init__(self, element_type, length='None'):
+    def __init__(self, element_type, length='None', doc_string=''):
         super(SequenceType, self).__init__('SEQUENCE')
         self.element_type = element_type
         self.length = length
+        self.doc_string = doc_string
 
     def __str__(self):
         return '%s of %s' % (super(SequenceType, self).__str__(), str(self.element_type))
@@ -173,27 +176,35 @@ def parse_coreml_feature_type(raw_type, batch_size=1):
     :return: one of our Int64Type, FloatType, StringType, Int64TensorType, FloatTensorType, or DictionaryType
     '''
     type_name = raw_type.WhichOneof('Type')
+    doc_string = raw_type.shortDescription
 
     if type_name == 'int64Type':
-        return Int64Type()
+        return Int64Type(doc_string=doc_string)
     elif type_name == 'doubleType':
-        return FloatType()
+        return FloatType(doc_string=doc_string)
     elif type_name == 'stringType':
-        return StringType()
+        return StringType(doc_string=doc_string)
     elif type_name == 'imageType':
         # Produce [C, H, W]-tensor, where C is the number of color channels, H the height, and W the width.
         color_space = raw_type.imageType.colorSpace
         shape = [batch_size]
         if color_space == 10:  # gray scale
             shape.append(1)
-        elif color_space in [20, 30]:  # RGB (20) or BGR (30)
+            doc_string = 'Image(s) in gray scale. If there are N images, it is a 4-D tensor with shape [N, 1, H, W]'
+        elif color_space == 20:  # RGB (20)
             shape.append(3)
+            doc_string = 'Image(s) in RGB format. It is a [N, C, H, W]-tensor. The 1st/2nd/3rd slices along the' \
+                         'C-axis are red, green, and blue channels, respectively.'
+        elif color_space == 30:  # BGR (30)
+            shape.append(3)
+            doc_string = 'Image(s) in BGR format. It is a [N, C, H, W]-tensor. The 1st/2nd/3rd slices along the' \
+                         'C-axis are blue, green, and red channels, respectively.'
         else:
-            raise RuntimeError('Invalid image color space')
+            raise RuntimeError('Unknown image format. Only gray-level, RGB, and BGR are supported')
         shape.append(raw_type.imageType.height)
         shape.append(raw_type.imageType.width)
         color_space_map = {10: 'GRAY', 20: 'RGB', 30: 'BGR'}
-        return FloatTensorType(shape, color_space_map[color_space])
+        return FloatTensorType(shape, color_space_map[color_space], doc_string=doc_string)
     elif type_name == 'multiArrayType':
         element_type_id = raw_type.multiArrayType.dataType
         shape = [d for d in raw_type.multiArrayType.shape]
@@ -206,23 +217,20 @@ def parse_coreml_feature_type(raw_type, batch_size=1):
         else:
             shape = [batch_size, 1]  # Missing shape information. We will try inferring it.
 
-        if element_type_id == 65568:
-            # CoreML FLOAT32
-            return FloatTensorType(shape)
-        elif element_type_id == 65600:
-            # CoreML DOUBLE
-            return FloatTensorType(shape)
+        if element_type_id in [65568, 65600]:
+            # CoreML FLOAT32 & DOUBLE
+            return FloatTensorType(shape, doc_string=doc_string)
         elif element_type_id == 131104:
             # CoreML INT32
-            return Int64TensorType(shape)
+            return Int64TensorType(shape, doc_string=doc_string)
         else:
             raise RuntimeError('Invalid element type')
     elif type_name == 'dictionaryType':
         key_type = raw_type.dictionaryType.WhichOneof('KeyType')
         if key_type == 'int64KeyType':
-            return DictionaryType(Int64Type(), FloatType())
+            return DictionaryType(Int64Type(), FloatType(), doc_string=doc_string)
         elif key_type == 'stringKeyType':
-            return DictionaryType(StringType(), FloatType())
+            return DictionaryType(StringType(), FloatType(), doc_string=doc_string)
         else:
             raise RuntimeError('Unsupported key type: {}'.format(key_type))
     else:
