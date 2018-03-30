5327651fc176c1edf72ccb1cc79b5bd385275b89
Wei-Sheng Chin <wschin@outlook.com>
2018-03-30 09:24:35 -0700

    Let binarizer always produce floats

--- onnxmltools/convert/sklearn/operator_converters/Binarizer.py
+++ onnxmltools/convert/sklearn/operator_converters/Binarizer.py
@@ -9,12 +9,7 @@ from .common import concatenate_variables
 
 
 def convert_sklearn_binarizer(scope, operator, container):
-    if len(operator.inputs) > 1:
-        # If there are multiple input tensors, we combine them using a FeatureVectorizer
-        feature_name = concatenate_variables(scope, operator.inputs, container)
-    else:
-        # No concatenation is needed, we just use the first variable's name
-        feature_name = operator.inputs[0].full_name
+    feature_name = concatenate_variables(scope, operator.inputs, container)
 
     op_type = 'Binarizer'
     attrs = {'name': scope.get_unique_operator_name(op_type), 'threshold': float(operator.raw_operator.threshold)}
