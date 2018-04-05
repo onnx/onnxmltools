1085db3c3cf09a555317a7ee3591e76faba5f20f
Wei-Sheng Chin <wschin@outlook.com>
2018-04-05 15:22:51 -0700

    Follow C-style naming required by ONNX

--- onnxmltools/convert/common/_topology.py
+++ onnxmltools/convert/common/_topology.py
@@ -4,6 +4,8 @@
 # license information.
 # --------------------------------------------------------------------------
 
+import re
+import warnings
 from ...proto import helper
 from .data_types import *
 from ._container import ModelComponentContainer
@@ -249,6 +251,18 @@ class Topology:
         :param existing_names: a set containing strings which cannot be produced
         :return: a string similar to the seed
         '''
+        if seed == '':
+            raise ValueError('Name seed must be an non-empty string')
+
+        # Make the seed meet C-style naming convention
+        original_seed = seed
+        seed = re.sub('[^0-9a-zA-Z]', '_', seed)  # Only alphabets and numbers are allowed
+        if re.match('^[0-9]', seed):  # The first symbol cannot be a number
+            seed = '_' + seed
+        if original_seed != seed:
+            warnings.warn('Illegal C-style name %s is replaced by %s' % (original_seed, seed))
+
+        # If seed has never been seen, we return it as it is. Otherwise, we will append an number to make it unique.
         if seed not in existing_names:
             existing_names.add(seed)
             return seed
