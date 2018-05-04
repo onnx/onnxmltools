e2f17bde632996e1d40810e21356c516f23f705e
Wei-Sheng Chin <wschin@outlook.com>
2018-05-04 15:05:14 -0700

    Add doc

--- onnxmltools/convert/coreml/operator_converters/neural_network/Reshape.py
+++ onnxmltools/convert/coreml/operator_converters/neural_network/Reshape.py
@@ -9,13 +9,33 @@ from ....common._registration import register_converter
 
 
 def apply_reshape(scope, input_name, output_name, desired_shape, container, operator_name=None):
+    '''
+    This function create a Reshape of op_version=5 to adjust the tensor indicated by input_name and put the result onto
+    the tensor specified by output_name.
+
+    :param scope: The Scope object we want to allocate this Reshape and its input
+    :param input_name: A string, the name of the tensor we want to reshape
+    :param output_name: A string, the name of the tensor for storing reshaped result
+    :param desired_shape: A list of integers, indicating the targeted shape
+    :param container: A ModelComponentContainer object used to collect all ONNX objects
+    :param operator_name: The name of the ONNX Reshape we are going to create. If not specified, we may create one.
+    :return:
+    '''
+    # The shape attribute of Reshape becomes a tensor input, so we create one tensor to store that attribute.
     desired_shape_name = scope.get_unique_variable_name('shape_tensor')
     container.add_initializer(desired_shape_name, onnx_proto.TensorProto.INT64, [len(desired_shape)], desired_shape)
+
+    # Create the name of ONNX Reshape if not specified
     if operator_name is None:
-        name = scope.get_unique_variable_name('Reshape')
+        name = scope.get_unique_operator_name('Reshape')
     else:
         name = operator_name
+
+    # Create ONNX Reshape operator
     container.add_node('Reshape', [input_name, desired_shape_name], output_name, op_version=5, name=name)
+
+    # Return shape of the output. All other ONNX operator creator should do the same.
+    return desired_shape
 
 
 def convert_reshape(scope, operator, container):
