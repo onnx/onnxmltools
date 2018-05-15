d16375f11b96723d91d692e6b8d56d1d0c3ed95f
Wei-Sheng Chin <wschin@outlook.com>
2018-05-14 17:56:06 -0700

    Replace Crop with Slice because Crop will leave

--- onnxmltools/convert/coreml/operator_converters/neural_network/Crop.py
+++ onnxmltools/convert/coreml/operator_converters/neural_network/Crop.py
@@ -4,6 +4,7 @@
 # license information.
 # --------------------------------------------------------------------------
 
+import numpy as np
 from ....common._registration import register_converter
 
 
@@ -11,15 +12,29 @@ def convert_crop(scope, operator, container):
     if len(operator.input_full_names) > 2:
         raise RuntimeError('Unlike CoreML, ONNX only supports cropping with a single input')
 
-    op_type = 'Crop'
+    op_type = 'Slice'
     attrs = {'name': operator.full_name}
-    border = operator.raw_operator.crop.cropAmounts.borderAmounts
-    left = border[1].startEdgeSize
-    top = border[0].startEdgeSize
-    right = border[1].endEdgeSize
-    bottom = border[0].endEdgeSize
+    n_dims = len(operator.inputs[0].type.shape)
+    attrs['axes'] = list(i for i in range(n_dims))
 
-    attrs['border'] = [left, top, right, bottom]
+    # Mode 1: only one input is provided
+    if len(operator.inputs) == 1:
+        H = operator.inputs[0].type.shape[2]
+        W = operator.inputs[0].type.shape[3]
+        border = operator.raw_operator.crop.cropAmounts.borderAmounts
+        left = border[1].startEdgeSize
+        top = border[0].startEdgeSize
+        right = border[1].endEdgeSize
+        bottom = border[0].endEdgeSize
+        attrs['starts'] = [0, 0, top, left]
+        attrs['ends'] = [np.iinfo(np.int32).max, np.iinfo(np.int32).max, H - bottom, W - right]
+    else:
+        H_ref = operator.inputs[0].type.shape[2]
+        W_ref = operator.inputs[0].type.shape[3]
+        offset_h = operator.raw_operator.crop.offset[0]
+        offset_w = operator.raw_operator.crop.offset[1]
+        attrs['starts'] = [0, 0, offset_h, offset_w]
+        attrs['ends'] = [np.iinfo(np.int32).max, np.iinfo(np.int32).max, H_ref + offset_h, W_ref + offset_w]
 
     container.add_node(op_type, operator.input_full_names, operator.output_full_names, **attrs)
 
