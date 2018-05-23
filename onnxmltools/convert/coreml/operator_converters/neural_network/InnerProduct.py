5691f55f74f22faf92742b98e7daa9b3d0d08020
Du Li <duli1@microsoft.com>
2018-05-23 16:01:57 -0700

    replace FC with GEMM

--- onnxmltools/convert/coreml/operator_converters/neural_network/InnerProduct.py
+++ onnxmltools/convert/coreml/operator_converters/neural_network/InnerProduct.py
@@ -6,32 +6,40 @@
 
 from .....proto import onnx_proto
 from ....common._registration import register_converter
-
+from ....common._apply_operation import apply_reshape
 
 def convert_inner_product(scope, operator, container):
     params = operator.raw_operator.innerProduct
-    op_type = 'FC'
+    op_type = 'Gemm'
     inputs = [variable.full_name for variable in operator.inputs]
     outputs = [variable.full_name for variable in operator.outputs]
     attrs = {'name': operator.full_name}
 
-    name_w = scope.get_unique_variable_name(operator.full_name + '_W')
-    shape_w = [params.outputChannels, params.inputChannels]
-    inputs.append(name_w)
-    container.add_initializer(name_w, onnx_proto.TensorProto.FLOAT, shape_w, params.weights.floatValue)
+    shape_a = operator.inputs[0].type.shape
+    reshaped_tensor_name = scope.get_unique_variable_name('reshape_output')
+    apply_reshape(scope, operator.inputs[0].full_name, reshaped_tensor_name, container, desired_shape=[-1, params.inputChannels])
+    inputs[0] = reshaped_tensor_name
 
     name_b = scope.get_unique_variable_name(operator.full_name + '_B')
-    shape_b = [params.outputChannels]
+    shape_b = [params.outputChannels, params.inputChannels]
     inputs.append(name_b)
-    if params.hasBias:
-        container.add_initializer(name_b, onnx_proto.TensorProto.FLOAT, shape_b, params.bias.floatValue)
-    else:
-        container.add_initializer(name_b, onnx_proto.TensorProto.FLOAT, shape_b, [0.] * shape_b[0])
+    container.add_initializer(name_b, onnx_proto.TensorProto.FLOAT, shape_b, params.weights.floatValue)
 
-    attrs['axis'] = 1
-    attrs['axis_w'] = 1
+    name_c = scope.get_unique_variable_name(operator.full_name + '_C')
+    shape_c = [params.outputChannels]
+    inputs.append(name_c)
+    if params.hasBias:
+        container.add_initializer(name_c, onnx_proto.TensorProto.FLOAT, shape_c, params.bias.floatValue)
+    else:
+        container.add_initializer(name_b, onnx_proto.TensorProto.FLOAT, shape_c, [0.] * shape_b[0])
+    attrs['alpha'] = 1.0
+    attrs['beta'] = 1.0
+    attrs['transB'] = 1
+
+    #workaround for a CNTK GEMM bug
+    gemm_output_name = scope.get_unique_variable_name('gemm_output_name')
+    apply_reshape(scope, operator.outputs[0].full_name, gemm_output_name, container, desired_shape=[-1, params.outputChannels])
 
     container.add_node(op_type, inputs, outputs, **attrs)
-
 
 register_converter('innerProduct', convert_inner_product)
