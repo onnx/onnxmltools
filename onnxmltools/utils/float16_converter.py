bfffce07fa17a7b0e5e7229c97416cf6952c6378
Xuan Li <stevli@microsoft.com>
2018-06-07 12:11:51 -0700

    convert raw_data

--- onnxmltools/utils/float16_converter.py
+++ onnxmltools/utils/float16_converter.py
@@ -1,10 +1,16 @@
 import numpy as np
+from struct import unpack
 from ..convert.common.data_types import *
 from onnxmltools.utils import load_model
 
 
-def npfloat16_to_int(list):
-    return [int(bin(_.view('H'))[2:].zfill(16), 2) for _ in list]
+def npfloat16_to_int(np_list):
+    """
+    Convert numpy float16 to python int.
+    :param np_list: numpy float16 list
+    :return int_list: python int list
+    """
+    return [int(bin(_.view('H'))[2:].zfill(16), 2) for _ in np_list]
 
 
 def float16_converter(model_path):
@@ -18,7 +24,8 @@ def float16_converter(model_path):
     ::
 
         from onnxmltools.utils import float16_converter
-        float16_converter('c:/winmlperf_coreml_SqueezeNet_prerelease.onnx')
+        model_path = 'c:/winmlperf_coreml_SqueezeNet_prerelease.onnx'
+        model = float16_converter(model_path)
     """
     model = load_model(model_path)
     # create a queue for BFS
@@ -29,80 +36,78 @@ def float16_converter(model_path):
         next_level = []
         for q in queue:
             # if q is model, push q.graph
-            try: q.graph
-            except: pass
-            else: next_level.append(q.graph)
+            if hasattr(q, 'graph'):
+                next_level.append(q.graph)
             # if q is model.graph, push q.node.attribute
-            try: q.node
-            except: pass
-            else:
+            if hasattr(q, 'node'):
                 for n in q.node:
-                    try: n.attribute
-                    except: pass
-                    else: next_level.append(n.attribute)
+                    if hasattr(n, 'attribute'):
+                        next_level.append(n.attribute)
             # if q is model.graph.node.attribute, push q.g and q.graphs
-            try: q.g
-            except: pass
-            else: next_level.append(q.g)
+            if hasattr(q, 'g'):
+                next_level.append(q.g)
 
-            try: q.graphs
-            except: pass
-            else:
+            if hasattr(q, 'graphs'):
                 for n in q.graphs:
                     next_level.append(n)
             # graph.initializer  (TensorProto)
-            # for n in q.initializer:
-            try: q.initializer
-            except: pass
-            else:
+            if hasattr(q, 'initializer'):
                 for n in q.initializer:
                     if n.data_type == onnx_proto.TensorProto.FLOAT:
                         n.data_type = onnx_proto.TensorProto.FLOAT16
-                        n.int32_data[:] = npfloat16_to_int(np.float16(n.float_data))
+                        # convert float_data (float) to flat16 and write to int32_data
+                        int_list = npfloat16_to_int(np.float16(n.float_data))
+                        n.int32_data[:] = int_list
                         n.float_data[:] = []
-                        # TODO: raw_data
+                        # convert n.raw_data (bytes) to float
+                        float32_list = []
+                        for i in range(len(n.raw_data)//4):
+                            float32_list += unpack('f', n.raw_data[i*4:(i+1)*4])
+                        # convert float to flat16
+                        int_list = npfloat16_to_int(np.float16(float32_list))
+                        # convert float16 to bytes and write back to raw_data
+                        n.raw_data = b''
+                        for num in int_list:
+                            n.raw_data += num.to_bytes(2, byteorder='little', signed=True)
             # graph.input (ValueInfoProto)
-            try: q.input
-            except: pass
-            else:
+            if hasattr(q, 'input'):
                 for n in q.input:
                     # check if the input has type of ValueInfoProto (GraphProto) or string (NodeProto)
-                    try: n.type
-                    except: pass
-                    else:
+                    if hasattr(n, 'type'):
                          if n.type.tensor_type.elem_type == onnx_proto.TensorProto.FLOAT:
                             n.type.tensor_type.elem_type = onnx_proto.TensorProto.FLOAT16
             # graph.output (ValueInfoProto)
-            try: q.output
-            except: pass
-            else:
+            if hasattr(q, 'output'):
                 for n in q.output:
                     # check if the output has type of ValueInfoProto (GraphProto) or string (NodeProto)
-                    try: n.type
-                    except: pass
-                    else:
+                    if hasattr(n, 'type'):
                         if n.type.tensor_type.elem_type == onnx_proto.TensorProto.FLOAT:
                             n.type.tensor_type.elem_type = onnx_proto.TensorProto.FLOAT16
             # if q is graph, process graph.value_info (ValueInfoProto)
-            try: q.value_info
-            except: pass
-            else:
+            if hasattr(q, 'value_info'):
                 for n in q.value_info:
                     if n.type.tensor_type.elem_type == onnx_proto.TensorProto.FLOAT:
                         n.type.tensor_type.elem_type = onnx_proto.TensorProto.FLOAT16
             # if q is node.attribute, process node.attribute.t (TensorProto)
-            try: q.t
-            except: pass
-            else:
+            if hasattr(q, 't'):
                 if q.t.data_type == onnx_proto.TensorProto.FLOAT:
                     q.t.data_type = onnx_proto.TensorProto.FLOAT16
-                    q.t.int32_data[:] = npfloat16_to_int(np.float16(q.t.float_data))
+                    int_list = npfloat16_to_int(np.float16(q.t.float_data))
+                    q.t.int32_data[:] = int_list
                     q.t.float_data[:] = []
-                    # TODO: raw_data
+                    # convert q.t.raw_data (bytes) to float
+                    float32_list = []
+                    for i in range(len(q.t.raw_data) // 4):
+                        float32_list += unpack('f', q.t.raw_data[i * 4:(i + 1) * 4])
+                    # convert float to flat16
+                    int_list = npfloat16_to_int(np.float16(float32_list))
+                    # convert float16 to bytes and write back to raw_data
+                    q.t.raw_data = b''
+                    for num in int_list:
+                        q.t.raw_data += num.to_bytes(2, byteorder='little', signed=True)
+
             # if q is node.attribute, process node.attribute.tensors (TensorProto)
-            try: q.tensors
-            except: pass
-            else:
+            if hasattr(q, 'tensors'):
                 for n in q.tensors:
                     if n.data_type == onnx_proto.TensorProto.FLOAT:
                         n.data_type = onnx_proto.TensorProto.FLOAT16
