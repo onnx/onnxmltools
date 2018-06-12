4e873163df03c4bdbb362088fa348be4230b8385
stevenlix <38092805+stevenlix@users.noreply.github.com>
2018-06-11 23:19:05 -0700

    import float16_converter function

--- onnxmltools/utils/__init__.py
+++ onnxmltools/utils/__init__.py
@@ -11,4 +11,4 @@ from .main import set_model_version
 from .main import set_model_domain
 from .main import set_model_doc_string
 from .visualize import visualize_model
-from .float16_converter import float16_converter
+from .float16_converter import convert_float_to_float16
