e0e460cb979efbb15fc9245801caccf5f8bfa934
Wei-Sheng Chin <wechi@microsoft.com>
2018-06-18 16:41:10 -0700

    Reshape input of ZipMap if it is not 2-D.
    The first dimension is assumed to be the batch size, N.
    The the produce of all other dimensions are the feature size, C.
    For example, [N, C, H, W] will be reshaped to [N, C*H*W] before
    calling ZipMap.

--- onnxmltools/convert/coreml/operator_converters/TensorToProbabilityMap.py
+++ onnxmltools/convert/coreml/operator_converters/TensorToProbabilityMap.py
@@ -4,6 +4,8 @@
 # license information.
 # --------------------------------------------------------------------------
 
+import numbers
+from ...common._apply_operation import apply_reshape
 from ...common._registration import register_converter
 
 
@@ -39,7 +41,29 @@ def convert_tensor_to_probability_map(scope, operator, container):
     else:
         raise TypeError('Only neural network classifiers and pipeline classifiers are supported')
 
-    container.add_node('ZipMap', [operator.inputs[0].full_name], [operator.outputs[0].full_name],
+    input_shape = operator.inputs[0].type.shape
+    if len(operator.inputs[0].type.shape) != 2:
+        # Calculate the shape attribute of ONNX Reshape
+        if input_shape[0] != 'None':
+            N = input_shape[0]
+        else:
+            N = -1  # -1 means that this dimension is automatically determined in runtime and unknown in conversion time
+
+        if all(isinstance(i, numbers.Integral) for i in input_shape[1:]):
+            C = 1
+            for i in input_shape[1:]:
+                C *= int(i)
+        else:
+            C = -1  # -1 means that this dimension is automatically determined in runtime and unknown in conversion time
+
+        # ZipMap in ONNX only accepts [C] and [N, C] inputs. In cases of [N, C, 1, 1], we reshape the probability tensor
+        # into [N, C] before feeding it into ZipMap.
+        buffer_name = scope.get_unique_variable_name('buffer')
+        apply_reshape(scope, operator.inputs[0].full_name, buffer_name, container, desired_shape=[N, C])
+    else:
+        buffer_name = operator.inputs[0].full_name
+
+    container.add_node('ZipMap', buffer_name, operator.outputs[0].full_name,
                        op_domain='ai.onnx.ml', **attrs)
 
 
