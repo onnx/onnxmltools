a033418f2449c94d34223622a7db84d8adda4386
Wei-Sheng Chin <wschin@outlook.com>
2018-06-27 10:37:50 -0700

    Add two common scalers in scikit-learn (#101)

--- tests/sklearn/test_ScalerConverter.py
+++ tests/sklearn/test_ScalerConverter.py
@@ -2,7 +2,7 @@
 Tests scikit-learn's standard scaler converter.
 """
 import unittest
-from sklearn.preprocessing import StandardScaler, RobustScaler
+from sklearn.preprocessing import StandardScaler, RobustScaler, MinMaxScaler, MaxAbsScaler
 from onnxmltools import convert_sklearn
 from onnxmltools.convert.common.data_types import Int64TensorType, FloatTensorType
 
@@ -34,6 +34,18 @@ class TestSklearnScalerConverter(unittest.TestCase):
 
     def test_robust_scaler_floats_no_scaling(self):
         model = RobustScaler(with_scaling=False)
+        model.fit([[0., 0., 3.], [1., 1., 0.], [0., 2., 1.], [1., 0., 2.]])
+        model_onnx = convert_sklearn(model, 'scaler', [('input', FloatTensorType([1, 3]))])
+        self.assertTrue(model_onnx is not None)
+
+    def test_min_max_scaler(self):
+        model = MinMaxScaler()
+        model.fit([[0., 0., 3.], [1., 1., 0.], [0., 2., 1.], [1., 0., 2.]])
+        model_onnx = convert_sklearn(model, 'scaler', [('input', FloatTensorType([1, 3]))])
+        self.assertTrue(model_onnx is not None)
+
+    def test_max_abs_scaler(self):
+        model = MaxAbsScaler()
         model.fit([[0., 0., 3.], [1., 1., 0.], [0., 2., 1.], [1., 0., 2.]])
         model_onnx = convert_sklearn(model, 'scaler', [('input', FloatTensorType([1, 3]))])
         self.assertTrue(model_onnx is not None)
