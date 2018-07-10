473dc45f30e134257154b7d19b7dbfb7f6336745
Minsuk Kang <mika@microsoft.com>
2018-07-10 14:44:15 -0700

    shape change

--- onnxmltools/convert/sklearn/shape_calculators/LinearClassifier.py
+++ onnxmltools/convert/sklearn/shape_calculators/LinearClassifier.py
@@ -41,7 +41,7 @@ def calculate_sklearn_linear_classifier_output_shapes(operator):
     if all(isinstance(i, np.ndarray) for i in class_labels):
         class_labels = np.concatenate(class_labels)
     if all(isinstance(i, (six.string_types, six.text_type)) for i in class_labels):
-        operator.outputs[0].type = StringTensorType(shape=[N])
+        operator.outputs[0].type = StringTensorType(shape=[N, 1])
         if len(class_labels) > 2 and operator.type != 'SklearnLinearSVC':
             # For multi-class classifier, we produce a map for encoding the probabilities of all classes
             if operator.targeted_onnx_version < StrictVersion('1.2'):
@@ -52,7 +52,7 @@ def calculate_sklearn_linear_classifier_output_shapes(operator):
             # For binary classifier, we produce the probability of the positive class
             operator.outputs[1].type = FloatTensorType(shape=[N, len(class_labels)])
     elif all(isinstance(i, (numbers.Real, bool, np.bool_)) for i in class_labels):
-        operator.outputs[0].type = Int64TensorType(shape=[N])
+        operator.outputs[0].type = Int64TensorType(shape=[N, 1])
         if len(class_labels) > 2 and operator.type != 'SklearnLinearSVC':
             # For multi-class classifier, we produce a map for encoding the probabilities of all classes
             if operator.targeted_onnx_version < StrictVersion('1.2'):
