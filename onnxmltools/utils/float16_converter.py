94c9b7cf716071a30b8def73afdb58774b1e221f
stevenlix <Li.Steven@microsoft.com>
2018-07-13 14:19:31 -0700

    Float16 convert (#107)
    * check input validation
    
    * fix minior issues based on reviewer's feedback
    
    * add cast node for traditional ML ops
    
    * fix AppVeyor build failure issue
    
    * check onnx version for shape_inference
    
    * add new function convert_tensor_float_to_float16 to simplify the code

--- onnxmltools/utils/float16_converter.py
+++ onnxmltools/utils/float16_converter.py
@@ -1,12 +1,16 @@
-#-------------------------------------------------------------------------
+# -------------------------------------------------------------------------
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License. See License.txt in the project root for
 # license information.
-#--------------------------------------------------------------------------
+# --------------------------------------------------------------------------
 
 import numpy as np
+import itertools
 from struct import unpack
 from ..proto import onnx_proto
+from ..convert.common._container import ModelComponentContainer
+import onnx
+
 
 def _npfloat16_to_int(np_list):
     '''
@@ -18,9 +22,49 @@ def _npfloat16_to_int(np_list):
     return [int(bin(_.view('H'))[2:].zfill(16), 2) for _ in np_list]
 
 
+def convert_tensor_float_to_float16(tensor):
+    '''
+    Convert tensor float to float16.
+
+    :param tensor: TensorProto object
+    :return tensor_float16: converted TensorProto object
+
+    Example:
+
+    ::
+
+        from onnxmltools.utils.float16_converter import convert_tensor_float_to_float16
+        new_tensor = convert_tensor_float_to_float16(tensor)
+
+    '''
+    if not isinstance(tensor, onnx_proto.TensorProto):
+        raise ValueError('Expected input type is an ONNX TensorProto but got %s' % type(tensor))
+
+    if tensor.data_type == onnx_proto.TensorProto.FLOAT:
+        tensor.data_type = onnx_proto.TensorProto.FLOAT16
+        # convert float_data (float type) to float16 and write to int32_data
+        if tensor.float_data:
+            int_list = _npfloat16_to_int(np.float16(tensor.float_data))
+            tensor.int32_data[:] = int_list
+            tensor.float_data[:] = []
+        # convert raw_data (bytes type)
+        if tensor.raw_data:
+            # convert n.raw_data to float
+            float32_list = []
+            for i in range(len(tensor.raw_data) // 4):
+                float32_list += unpack('f', tensor.raw_data[i * 4:(i + 1) * 4])
+            # convert float to float16
+            int_list = _npfloat16_to_int(np.float16(float32_list))
+            # convert float16 to bytes and write back to raw_data
+            tensor.raw_data = b''
+            for num in int_list:
+                tensor.raw_data += num.to_bytes(2, byteorder='little', signed=False)
+    return tensor
+
+
 def convert_float_to_float16(model):
     '''
-    Convert TensorProto DataType float in the ONNX ModelProto input to float16.
+    Convert tensor float type in the ONNX ModelProto input to tensor float16.
 
     :param model: ONNX ModelProto object
     :return: converted ONNX ModelProto object
@@ -41,11 +85,25 @@ def convert_float_to_float16(model):
         save_model(new_onnx_model, 'new_model.onnx')
 
     '''
-    # raise error if input is empty or not ModelProto object
-    if model is None or not isinstance(model, onnx_proto.ModelProto):
+    if onnx.__version__ >= '1.2':
+        from onnx.shape_inference import infer_shapes
+
+    domain_flag = 0
+    if not isinstance(model, onnx_proto.ModelProto):
         raise ValueError('Expected model type is an ONNX ModelProto but got %s' % type(model))
+
+    # create black list
+    op_black_list = ['ArrayFeatureExtractor', 'Binarizer', 'CastMap', 'CategoryMapper', 'DictVectorizer',
+                     'FeatureVectorizer', 'Imputer', 'LabelEncoder', 'LinearClassifier', 'LinearRegressor', 'Normalizer',
+                     'OneHotEncoder', 'SVMClassifier', 'SVMRegressor', 'Scaler', 'TreeEnsembleClassifier',
+                     'TreeEnsembleRegressor', 'ZipMap']
     # create a queue for BFS
     queue = []
+    value_info_list = []
+    node_list = []
+    # type inference on input model
+    if onnx.__version__ >= '1.2':
+        model = infer_shapes(model)
     queue.append(model)
     while queue:
         next_level = []
@@ -56,66 +114,82 @@ def convert_float_to_float16(model):
             # if q is model.graph, push q.node.attribute (AttributeProto)
             if isinstance(q, onnx_proto.GraphProto):
                 for n in q.node:
-                    # if hasattr(n, 'attribute'):
-                    next_level.append(n.attribute)
+                    # if n is in the black list (doesn't support float16), no conversion for the node,
+                    # and save the node for further processing
+                    if n.op_type in op_black_list:
+                        node_list.append(n)
+                    else:
+                        next_level.append(n.attribute)
             # if q is model.graph.node.attribute, push q.g and q.graphs (GraphProto)
-            if isinstance(q, onnx_proto.AttributeProto):  # and hasattr(q, 'g'):
+            if isinstance(q, onnx_proto.AttributeProto):
                 next_level.append(q.g)
                 for n in q.graphs:
                     next_level.append(n)
             # if q is graph, process graph.initializer(TensorProto), input, output and value_info (ValueInfoProto)
             if isinstance(q, onnx_proto.GraphProto):
-                for n in q.initializer:
-                    if n.data_type == onnx_proto.TensorProto.FLOAT:
-                        n.data_type = onnx_proto.TensorProto.FLOAT16
-                        # convert float_data (float) to float16 and write to int32_data
-                        if n.float_data:
-                            int_list = _npfloat16_to_int(np.float16(n.float_data))
-                            n.int32_data[:] = int_list
-                            n.float_data[:] = []
-                        if n.raw_data:
-                            # convert n.raw_data (bytes) to float
-                            float32_list = []
-                            for i in range(len(n.raw_data)//4):
-                                float32_list += unpack('f', n.raw_data[i*4:(i+1)*4])
-                            # convert float to float16
-                            int_list = _npfloat16_to_int(np.float16(float32_list))
-                            # convert float16 to bytes and write back to raw_data
-                            n.raw_data = b''
-                            for num in int_list:
-                                n.raw_data += num.to_bytes(2, byteorder='little', signed=False)
-                for n in q.input:
+                for n in q.initializer:  # TensorProto type
+                    n = convert_tensor_float_to_float16(n)
+                # for all ValueInfoProto with tensor(float) type in input, output and value_info, convert them to
+                # tensor(float16) except map and seq(map). And save them in value_info_list for further processing
+                for n in itertools.chain(q.input, q.output, q.value_info):
                     if n.type.tensor_type.elem_type == onnx_proto.TensorProto.FLOAT:
                         n.type.tensor_type.elem_type = onnx_proto.TensorProto.FLOAT16
-                for n in q.output:
-                    if n.type.tensor_type.elem_type == onnx_proto.TensorProto.FLOAT:
-                        n.type.tensor_type.elem_type = onnx_proto.TensorProto.FLOAT16
-                for n in q.value_info:
-                    if n.type.tensor_type.elem_type == onnx_proto.TensorProto.FLOAT:
-                        n.type.tensor_type.elem_type = onnx_proto.TensorProto.FLOAT16
+                        value_info_list.append(n)
             # if q is node.attribute, process node.attribute.t and node.attribute.tensors (TensorProto)
             if isinstance(q, onnx_proto.AttributeProto):
-                if q.t.data_type == onnx_proto.TensorProto.FLOAT:
-                    q.t.data_type = onnx_proto.TensorProto.FLOAT16
-                    if q.t.float_data:
-                        int_list = _npfloat16_to_int(np.float16(q.t.float_data))
-                        q.t.int32_data[:] = int_list
-                        q.t.float_data[:] = []
-                    if q.t.raw_data:
-                        # convert q.t.raw_data (bytes) to float
-                        float32_list = []
-                        for i in range(len(q.t.raw_data) // 4):
-                            float32_list += unpack('f', q.t.raw_data[i * 4:(i + 1) * 4])
-                        # convert float to float16
-                        int_list = _npfloat16_to_int(np.float16(float32_list))
-                        # convert float16 to bytes and write back to raw_data
-                        q.t.raw_data = b''
-                        for num in int_list:
-                            q.t.raw_data += num.to_bytes(2, byteorder='little', signed=False)
-                for n in q.tensors:
-                    if n.data_type == onnx_proto.TensorProto.FLOAT:
-                        n.data_type = onnx_proto.TensorProto.FLOAT16
+                for n in itertools.chain(q.t, q.tensors):
+                    n = convert_tensor_float_to_float16(n)
         queue = next_level
 
+    # process the nodes in black list that doesn't support tensor(float16)
+    for node in node_list:
+        # if input's name is in the value_info_list meaning input is tensor(float16) type, insert a Cast node
+        # before the node, change current node's input name and create new value_info for the new name
+        for i in range(len(node.input)):
+            input = node.input[i]
+            for value_info in value_info_list:
+                if input == value_info.name:
+                    # create new value_info for current node's new input name
+                    new_value_info = onnx_proto.ValueInfoProto()
+                    new_value_info.CopyFrom(value_info)
+                    new_value_info.name = input + '_casted'
+                    new_value_info.type.tensor_type.elem_type = onnx_proto.TensorProto.FLOAT
+                    model.graph.value_info._values.append(new_value_info)
+                    # add Cast node (from tensor(float16) to tensor(float) before current node
+                    container = ModelComponentContainer(onnx.__version__)
+                    attrs = {'name': input + 'Cast'}
+                    attrs['to'] = onnx_proto.TensorProto.FLOAT
+                    container.add_node('Cast', input, input + '_casted', op_domain='', op_version=7, **attrs)
+                    model.graph.node.extend(container.nodes)
+                    # change current node's input name
+                    node.input[i] = input + '_casted'
+                    domain_flag = 1
+                    continue
+        # if output's name is in the value_info_list meaning output is tensor(float16) type, insert a float16 to
+        # float Cast node after the node, change current node's output name and create new value_info for the new name
+        for i in range(len(node.output)):
+            output = node.output[i]
+            for value_info in value_info_list:
+                if output == value_info.name:
+                    # create new value_info for current node's new output
+                    new_value_info = onnx_proto.ValueInfoProto()
+                    new_value_info.CopyFrom(value_info)
+                    new_value_info.name = output + '_casted'
+                    new_value_info.type.tensor_type.elem_type = onnx_proto.TensorProto.FLOAT
+                    model.graph.value_info._values.append(new_value_info)
+                    # add Cast node (from tensor(float) to tensor(float16) after current node
+                    container = ModelComponentContainer(onnx.__version__)
+                    attrs = {'name': output + 'Cast'}
+                    attrs['to'] = onnx_proto.TensorProto.FLOAT16
+                    container.add_node('Cast', output + '_casted', output, op_domain='', op_version=7, **attrs)
+                    model.graph.node.extend(container.nodes)
+                    # change current node's input name
+                    node.output[i] = output + '_casted'
+                    domain_flag = 1
+                    continue
+    if domain_flag:
+        # Create operator set for cast node
+        op_set = model.opset_import.add()
+        op_set.domain = ""
+        op_set.version = 7
     return model
-
