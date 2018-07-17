3597f63218d5ee6f6f3d8d37ff2e10e5feb6d4ec
Wenbing Li <wenbingl@outlook.com>
2018-07-17 10:12:49 -0700

    Fix the keras reshape op conversion. (#110)

--- onnxmltools/convert/keras/operator_converters/Reshape.py
+++ onnxmltools/convert/keras/operator_converters/Reshape.py
@@ -12,7 +12,7 @@ from ...common._registration import register_converter
 def convert_keras_reshape(scope, operator, container):
     op = operator.raw_operator
     apply_reshape(scope, operator.inputs[0].full_name, operator.outputs[0].full_name, container,
-                  desired_shape=op.target_shape)
+                  desired_shape=[-1, ] + list(op.target_shape))
 
 
 register_converter(Reshape, convert_keras_reshape)
