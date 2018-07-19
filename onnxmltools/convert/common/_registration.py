023449083129199399fba1f23fb6fcb07f120e95
Wenbing Li <wenbingl@outlook.com>
2018-07-18 17:07:51 -0700

    more polish.

--- onnxmltools/convert/common/_registration.py
+++ onnxmltools/convert/common/_registration.py
@@ -14,8 +14,6 @@ _converter_pool = {}
 # is the callable object used to infer the output shape(s) for the operator specified by the key.
 _shape_calculator_pool = {}
 
-def default_shape_calculator(operator):
-    pass
 
 def register_converter(operator_name, conversion_function, overwrite=False):
     '''
@@ -28,12 +26,6 @@ def register_converter(operator_name, conversion_function, overwrite=False):
         raise ValueError('We do not overwrite registrated converter by default')
     _converter_pool[operator_name] = conversion_function
 
-def unregister_converter(operator_name):
-    '''
-    Unregister a converter function from the pool.
-    '''
-    if not _converter_pool.pop(operator_name):
-        raise ValueError('Unregister non-existing operator {}'.format(str(operator_name)))
 
 def get_converter(operator_name):
     '''
@@ -50,7 +42,7 @@ def get_converter(operator_name):
     return _converter_pool[operator_name]
 
 
-def register_shape_calculator(operator_name, calculator_function=default_shape_calculator, overwrite=False):
+def register_shape_calculator(operator_name, calculator_function, overwrite=False):
     '''
     :param operator_name: A unique operator ID. It is usually a string but you can use a type as well
     :param calculator_function: A callable object
@@ -61,13 +53,6 @@ def register_shape_calculator(operator_name, calculator_function=default_shape_c
         raise ValueError('We do not overwrite registrated shape calculator by default')
     _shape_calculator_pool[operator_name] = calculator_function
 
-
-def unregister_shape_calculator(operator_name):
-    '''
-    Unregister a converter shape function from the pool.
-    '''
-    if not _shape_calculator_pool.pop(operator_name):
-        raise ValueError('Unregister non-existing operator {}'.format(str(operator_name)))
 
 def get_shape_calculator(operator_name):
     '''
--- onnxmltools/convert/common/interface.py
+++ onnxmltools/convert/common/interface.py
@@ -1,4 +1,14 @@
-import abc,six
+# -------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for
+# license information.
+# --------------------------------------------------------------------------
+# This file defines the interface of the converter internal object for callback,
+# So the usage of the methods and properties list here will not be affected among the different versions.
+
+
+import abc
+import six
 
 @six.add_metaclass(abc.ABCMeta)
 class ModelContainer:
@@ -6,19 +16,19 @@ class ModelContainer:
 
     @abc.abstractmethod
     def add_initializer(self, name, onnx_type, shape, content):
-        '''
+        """
         Add a TensorProto into the initializer list of the final ONNX model
 
         :param name: Variable name in the produced ONNX model.
         :param onnx_type: Element types allowed in ONNX tensor, e.g., TensorProto.FLOAT and TensorProto.STRING.
         :param shape: Tensor shape, a list of integers.
         :param content: Flattened tensor values (i.e., a float list or a float array).
-        '''
+        """
         return
 
     @abc.abstractmethod
     def add_node(self, op_type, inputs, outputs, op_domain='', op_version=1, **attrs):
-        '''
+        """
         Add a NodeProto into the node list of the final ONNX model. If the input operator's domain-version information
         cannot be found in our domain-version pool (a Python set), we may add it.
 
@@ -28,7 +38,7 @@ class ModelContainer:
         :param op_domain: The domain name (e.g., ai.onnx.ml) of the operator we are trying to add.
         :param op_version: The version number (e.g., 0 and 1) of the operator we are trying to add.
         :param attrs: A Python dictionary. Keys and values are attributes' names and attributes' values, respectively.
-        '''
+        """
         return
 
 @six.add_metaclass(abc.ABCMeta)
@@ -38,33 +48,33 @@ class OperatorBase:
     @property
     @abc.abstractmethod
     def full_name(self):
-        '''
+        """
         Return a globally unique operator ID
-        '''
+        """
         pass
 
     @property
     @abc.abstractmethod
     def input_full_names(self):
-        '''
+        """
         Return all input variables' names
-        '''
+        """
         pass
 
     @property
     @abc.abstractmethod
     def output_full_names(self):
-        '''
+        """
         Return all outpu variables' names
-        '''
+        """
         pass
 
     @property
     @abc.abstractmethod
     def original_operator(self):
-        '''
+        """
         Return the original operator/layer
-        '''
+        """
         pass
 
 @six.add_metaclass(abc.ABCMeta)
