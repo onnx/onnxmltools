375713b1f1b9a5b5b98a1de394944fabddd07bc7
wenbingl <wenbingl@outlook.com>
2018-07-20 14:39:30 -0700

    code polish

--- onnxmltools/convert/keras/operator_converters/Pool.py
+++ onnxmltools/convert/keras/operator_converters/Pool.py
@@ -7,8 +7,8 @@
 from distutils.version import StrictVersion
 from keras.layers import MaxPooling1D, MaxPooling2D, MaxPooling3D, AveragePooling1D, AveragePooling2D, AveragePooling3D
 from keras.layers import GlobalMaxPooling1D, GlobalMaxPooling2D, GlobalAveragePooling1D, GlobalAveragePooling2D
-from ...common._apply_operation import apply_transpose
 from ...common._apply_operation import apply_reshape
+from ...common._apply_operation import apply_transpose
 from ...common._registration import register_converter
 from .common import get_permutation_config
 
@@ -49,12 +49,14 @@ def convert_keras_pooling_core(scope, operator, container, is_global, n_dims,
         else:
             raise RuntimeError("Unsupported padding type '{0}'".format(op.padding))
 
-    output_node_name = scope.get_unique_variable_name('pooling_for_reshape') if is_global else operator.outputs[0].full_name
+    # The output_tensor_name is used to store the Keras result produced by ONNX operators.
+    # For global pooling, a Reshape op is needed to match the actual Keras's output shape.
+    output_tensor_name = scope.get_unique_variable_name('pooling_for_reshape') if is_global else operator.outputs[0].full_name
 
     if channels_first:
         # In this case, the output of our Pool operator just match what Keras produces.
         container.add_node(op_type_prefix + onnx_op_type, adjusted_pooling_input,
-                           output_node_name, op_version=op_version, **attrs)
+                           output_tensor_name, op_version=op_version, **attrs)
     else:
         # Put the output of Pool operator to an intermediate tensor. Laster we will apply a Transpose to match the
         # original Keras output format
@@ -63,10 +65,10 @@ def convert_keras_pooling_core(scope, operator, container, is_global, n_dims,
                            op_version=op_version, **attrs)
 
         # Generate a final Transpose
-        apply_transpose(scope, pooling_output_name, output_node_name, container, perm=output_perm_axes)
+        apply_transpose(scope, pooling_output_name, output_tensor_name, container, perm=output_perm_axes)
 
     if is_global:
-        apply_reshape(scope, output_node_name,
+        apply_reshape(scope, output_tensor_name,
                       operator.outputs[0].full_name, container, desired_shape=operator.outputs[0].type.shape)
 
 
