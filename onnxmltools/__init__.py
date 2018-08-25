0cd78a742837aa429c57d3a54f269a2421770cca
Wenbing Li <wenbingl@outlook.com>
2018-08-24 17:31:07 -0700

    upgrade the version for the final 1.2.2 release. (#132)

--- onnxmltools/__init__.py
+++ onnxmltools/__init__.py
@@ -9,7 +9,7 @@ Main entry point to onnxmltools.
 This framework converts any machine learned model into onnx format
 which is a common language to describe any machine learned model.
 """
-__version__ = "1.2.0.0116"
+__version__ = "1.2.2.0129"
 __author__ = "Microsoft"
 __producer__ = "OnnxMLTools"
 __producer_version__ = __version__
