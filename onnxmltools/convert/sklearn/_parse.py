3a3b826c7febac3d757529ce338a6663b7bc6fd9
Prabhat Roy <prroy@microsoft.com>
2018-10-04 09:25:24 -0700

    Added KNN Classifier converter

--- onnxmltools/convert/sklearn/_parse.py
+++ onnxmltools/convert/sklearn/_parse.py
@@ -37,6 +37,7 @@ from sklearn.tree import DecisionTreeRegressor
 from sklearn.svm import SVC, SVR, NuSVC, NuSVR
 
 # K-nearest neighbors
+from sklearn.neighbors import KNeighborsClassifier
 from sklearn.neighbors import KNeighborsRegressor
 
 # Naive Bayes
@@ -65,7 +66,8 @@ from lightgbm import LGBMClassifier, LGBMRegressor
 # one output for everything not in the list.
 sklearn_classifier_list = [LogisticRegression, SGDClassifier, LinearSVC, SVC, NuSVC,
                            GradientBoostingClassifier, RandomForestClassifier, DecisionTreeClassifier,
-                           ExtraTreesClassifier, LGBMClassifier, BernoulliNB, MultinomialNB]
+                           ExtraTreesClassifier, LGBMClassifier, BernoulliNB, MultinomialNB,
+                           KNeighborsClassifier]
 
 # Associate scikit-learn types with our operator names. If two scikit-learn models share a single name, it means their
 # are equivalent in terms of conversion.
@@ -97,6 +99,7 @@ sklearn_operator_name_map = {RobustScaler: 'SklearnRobustScaler',
                              ExtraTreesRegressor: 'SklearnExtraTreesRegressor',
                              GradientBoostingClassifier: 'SklearnGradientBoostingClassifier',
                              GradientBoostingRegressor: 'SklearnGradientBoostingRegressor',
+                             KNeighborsClassifier: 'SklearnKNeighborsClassifier',
                              KNeighborsRegressor: 'SklearnKNeighborsRegressor',
                              MultinomialNB: 'SklearnMultinomialNB',
                              BernoulliNB: 'SklearnBernoulliNB',
