eb41c2ca790b1d8ec601057c57b0cdd2c623e161
Jilong Liao <jilli@microsoft.com>
2018-10-17 16:55:31 -0700

    binary lgbm classifier only

--- tests/sklearn/test_LightGBMClassifier.py
+++ tests/sklearn/test_LightGBMClassifier.py
@@ -15,14 +15,7 @@ class TestLGBMClassifierConverter(unittest.TestCase):
         model.fit(X, y)
         return model
 
-    def _fit_model_multiclass_classification(self, model):
-        iris = datasets.load_iris()
-        X = iris.data[:, :3]
-        y = iris.target
-        model.fit(X, y)
-        return model
-
     def test_model_binary_classification(self):
         model = self._fit_model_binary_classification(LGBMClassifier(objective="binary", boosting_type="gbdt"))
-        model_onnx = convert_sklearn(model, 'scikit-learn LGBM classifier', [('input', FloatTensorType([1, 3]))])
+        model_onnx = convert_sklearn(model, 'scikit-learn LGBM binary classifier', [('input', FloatTensorType([1, 3]))])
         self.assertIsNotNone(model_onnx)
