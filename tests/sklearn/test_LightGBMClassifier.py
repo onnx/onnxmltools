74263ff75aaa2eb48f4737218be024e71fcc53fb
Jilong Liao <jilli@microsoft.com>
2018-10-17 17:17:58 -0700

    add regression + multiclass

--- tests/sklearn/test_LightGBMClassifier.py
+++ tests/sklearn/test_LightGBMClassifier.py
@@ -1,6 +1,7 @@
 import unittest
 from sklearn import datasets
 from lightgbm import LGBMClassifier
+from lightgbm import LGBMRegressor
 
 from onnxmltools import convert_sklearn
 from onnxmltools.convert.common.data_types import FloatTensorType
@@ -15,7 +16,41 @@ class TestLGBMClassifierConverter(unittest.TestCase):
         model.fit(X, y)
         return model
 
+    def _fit_model_multiclass_classification(self, model):
+        digits = datasets.load_digits()
+        X = digits.data[:, :10]
+        y = digits.target
+        model.fit(X, y)
+        return model
+
+    def _fit_model_regression(self, model):
+        diabetes = datasets.load_digits()
+        X = diabetes.data
+        y = diabetes.target
+        model.fit(X, y)
+        return model
+
     def test_model_binary_classification(self):
-        model = self._fit_model_binary_classification(LGBMClassifier(objective="binary", boosting_type="gbdt"))
+        model = self._fit_model_binary_classification(LGBMClassifier(
+            objective="binary",
+            learning_rate=0.05,
+            boosting_type="gbdt"))
         model_onnx = convert_sklearn(model, 'scikit-learn LGBM binary classifier', [('input', FloatTensorType([1, 3]))])
+        self.assertIsNotNone(model_onnx)
+
+    def test_model_multiclass_classification(self):
+        model = self._fit_model_binary_classification(LGBMClassifier(
+            objective="ova", 
+            learning_rate=0.05, 
+            boosting_type="gbdt", 
+            num_class=10))
+        model_onnx = convert_sklearn(model, 'scikit-learn LGBM multiclass classifier', [('input', FloatTensorType([1, 10]))])
+        self.assertIsNotNone(model_onnx)
+
+    def test_model_regression(self):
+        model = self._fit_model_regression(LGBMRegressor(
+            objective="regression",
+            boosting_type="gbdt",
+            metric="rmsle"))
+        model_onnx = convert_sklearn(model, 'scikit-learn LGBM regression', [('input', FloatTensorType([1, 10]))])
         self.assertIsNotNone(model_onnx)
