20ff1ec730f479c7edbccd46db050a1af1cabd8b
Wenbing Li <wenli@microsoft.com>
2018-11-06 22:32:17 -0800

    add nchw input conversion support.

--- onnxmltools/convert/common/optimizer.py
+++ onnxmltools/convert/common/optimizer.py
@@ -9,10 +9,14 @@ from onnxmltools.proto import onnx_proto, helper
 
 class LinkedNode(object):
 
-    def __init__(self, node=None):
+    def __init__(self, node=None, in_n=None, out_n=None):
         self.origin = node  # type: onnx_proto.NodeProto
-        self.input = {} if node is None else {i_: i_ for i_ in node.input}
-        self.output = {} if node is None else {o_: o_ for o_ in node.output}
+        if in_n is None and node is not None:
+            in_n = node.input
+        if out_n is None and node is not None:
+            out_n = node.output
+        self.input = {} if in_n is None else {i_: i_ for i_ in in_n}
+        self.output = {} if out_n is None else {o_: o_ for o_ in out_n}
         self.precedence = []
         self.successor = []
         self.attributes = {}
@@ -43,9 +47,33 @@ class LinkedNode(object):
         return next(value for (key, value) in six.iteritems(self.input))
 
     @property
+    def single_origin_input(self):
+        assert self.origin is not None and len(self.input) == 1
+        return self.origin.input[0]
+
+    @property
     def single_output(self):
         assert self.origin is not None and len(self.output) == 1
         return next(value for (key, value) in six.iteritems(self.output))
+
+    @property
+    def single_origin_output(self):
+        assert self.origin is not None and len(self.output) == 1
+        return self.origin.output[0]
+
+    def in_redirect(self, old_name, name):
+        if old_name in self.input:
+            self.input[old_name] = name
+        else:
+            key = next(k for k, v in six.iteritems(self.input) if v == old_name)
+            self.input[key] = name
+
+    def out_redirect(self, old_name, name):
+        if old_name in self.output:
+            self.output[old_name] = name
+        else:
+            key = next(k for k, v in six.iteritems(self.output) if v == old_name)
+            self.output[key] = name
 
     def generate(self):
         if not self.input and not self.output and not self.attributes:
@@ -70,9 +98,10 @@ class LinkedNode(object):
     def add_precedence(self, pre, tname):
         self.precedence.append(pre)
         pre.successor.append(self)
+        assert tname in self.input.values() and tname in pre.output.values()
 
     @staticmethod
-    def build_from_onnx(onnx_nodes):
+    def build_from_onnx(onnx_nodes, nchw_inputs):
         view = []
         var_map = {}
         for o_ in onnx_nodes:
@@ -80,21 +109,37 @@ class LinkedNode(object):
             view.append(ln)
             for var_ in o_.output:
                 assert var_map.get(var_) is None
-                var_map.setdefault(var_, ln)
+                var_map[var_] = ln
 
+        additional_nodes = []
         for n_ in view:
             for var_ in n_.origin.input:
                 target = var_map.get(var_)
                 if target is None:
-                    target = LinkedNode()  # create an empty node as input
+                    target = LinkedNode(out_n=[var_])  # create an empty node as input
+                    new_output = var_ + '_nhwc'
+                    if var_ in nchw_inputs:
+                        nnode = LinkedNode(
+                            helper.make_node(
+                            'Transpose',
+                            [var_],
+                            [new_output],
+                            perm=[0, 2, 3, 1]))
+                        var_map[new_output] = nnode
+                        nnode.add_precedence(target, var_)
+                        n_.in_redirect(var_, new_output)
+                        target = nnode
+                        var_ = new_output
+                        additional_nodes.append(nnode)
+
                 n_.add_precedence(target, var_)
 
         for n_ in view:  # add a dummy output node.
             if len(n_.successor) < 1:
                 for var_ in n_.origin.output:
-                    LinkedNode().add_precedence(n_, var_)
+                    LinkedNode(in_n=[var_]).add_precedence(n_, var_)
 
-        return view
+        return view + additional_nodes
 
 
 class Solution(object):
@@ -105,22 +150,26 @@ class Solution(object):
         self.end = end
 
     @staticmethod
-    def get_perm(attrs):
+    def get_perm(onode):
         try:
             return next(
-                helper.get_attribute_value(attr) for attr in attrs if attr.name == 'perm')
+                helper.get_attribute_value(attr) for attr in onode.attribute if attr.name == 'perm')
         except StopIteration:
             return None
 
     @staticmethod
-    def delete_node(node_list, begin, node, end):
+    def is_useless_transpose(perm):
+        return perm == list(six.moves.range(len(perm)))
+
+    @staticmethod
+    def delete_node(node_list, begin, node, end):  # type: ([],LinkedNode, LinkedNode, LinkedNode)->[]
         if end.in_or_out:
-            # if the end is output node, the output name will be kept to avoid the model output updating.
-            begin.output[begin.origin.output[0]] = node.single_output
+            # if the end is output node, the output name will be kept to avoid the model output name updating.
+            begin.out_redirect(node.single_input, node.single_output)
         else:
             target_var_name = node.single_input
-            assert target_var_name in begin.origin.output # since the output info never be updated, except the final.
-            end.input[node.origin.output[0]] = target_var_name
+            assert target_var_name in begin.output.values()  # since the output info never be updated, except the final.
+            end.in_redirect(node.single_output, target_var_name)
 
         begin.successor = [end if v_ == node else v_ for v_ in begin.successor]
         end.precedence = [begin if v_ == node else v_ for v_ in end.precedence]
@@ -141,11 +190,11 @@ class Solution(object):
 
 class MergeSolution(Solution):
     def apply(self, node_list):
-        perm0 = self.get_perm(self.begin_n.origin.attribute)
-        perm1 = self.get_perm(self.end_p.origin.attribute)
+        perm0 = self.get_perm(self.begin_n.origin)
+        perm1 = self.get_perm(self.end_p.origin)
         assert len(perm0) == len(perm1)
         perm_f = [perm0[idx] for idx in perm1]
-        if perm_f == list(six.moves.range(len(perm_f))):
+        if self.is_useless_transpose(perm_f):
             super(MergeSolution, self).apply(node_list)  # delete all transpose nodes
         else:
             node_list = self.delete_node(node_list, self.begin_n, self.end_p, self.end)
@@ -156,11 +205,6 @@ class MergeSolution(Solution):
 class RedundantOptimizer(object):
     def __init__(self):
         pass
-
-    @staticmethod
-    def is_useless_transpose(attrs):
-        perm = Solution.get_perm(attrs)
-        return perm == range(len(perm))
 
     def find(self, node_list):
         for n_ in node_list:
@@ -180,7 +224,7 @@ class RedundantOptimizer(object):
                 return solution
             elif n_.is_transpose and \
                 n_.in_single_path and \
-                    self.is_useless_transpose(n_.attribute):
+                    Solution.is_useless_transpose(Solution.get_perm(n_.origin)):
                 solution = Solution(n_.precedence[0], n_, n_, n_.successor[0])
                 return solution
             else:
@@ -217,13 +261,14 @@ def _build_onnx_model(node_list):
     return regenerated
 
 
-def optimize_onnx(onnx_nodes):
+def optimize_onnx(onnx_nodes, nchw_inputs=None):
     """
     Optimize onnx model by several approaches.
     :param onnx_nodes: the onnx node list in onnx model.
     :return:
     """
-    node_list = LinkedNode.build_from_onnx(onnx_nodes)
+    node_list = LinkedNode.build_from_onnx(onnx_nodes,
+                                           nchw_inputs if nchw_inputs else [])
     solution = _find_an_optimization(node_list)
     while solution:
         node_list = _apply_optimization(solution, node_list)
--- onnxmltools/convert/main.py
+++ onnxmltools/convert/main.py
@@ -18,14 +18,15 @@ def convert_coreml(model, name=None, initial_types=None, doc_string='', target_o
                    custom_conversion_functions, custom_shape_calculators)
 
 
-def convert_keras(model, name=None, initial_types=None, doc_string='', target_opset=None,
-                  targeted_onnx=onnx.__version__, custom_conversion_functions=None, custom_shape_calculators=None):
+def convert_keras(model, name=None, initial_types=None, doc_string='',
+                  target_opset=None, targeted_onnx=onnx.__version__,
+                  channel_first_inputs=None, custom_conversion_functions=None, custom_shape_calculators=None):
     if not utils.keras_installed():
         raise RuntimeError('keras is not installed. Please install it to use this feature.')
 
     from .keras.convert import convert
     return convert(model, name, initial_types, doc_string, target_opset, targeted_onnx,
-                   custom_conversion_functions, custom_shape_calculators)
+                   channel_first_inputs, custom_conversion_functions, custom_shape_calculators)
 
 
 def convert_lightgbm(model, name=None, initial_types=None, doc_string='', target_opset=None,
