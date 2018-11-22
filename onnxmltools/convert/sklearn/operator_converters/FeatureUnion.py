ae7fb1f0994e9ecc0b9181bcb5d9dd494ad64f23
Prabhat Roy <prroy@microsoft.com>
2018-11-22 11:19:36 -0800

    Added supported transformer list check

--- onnxmltools/convert/sklearn/operator_converters/FeatureUnion.py
+++ onnxmltools/convert/sklearn/operator_converters/FeatureUnion.py
@@ -9,19 +9,26 @@ from ...common._apply_operation import apply_abs, apply_cast, apply_mul, apply_r
 from ...common._registration import get_converter, register_converter
 from .._parse import sklearn_operator_name_map
 import numpy as np
+from sklearn.decomposition import PCA
+from sklearn.decomposition import TruncatedSVD
+
 
 class Oper:
-    def __init__(self, model, inputs, op_type):
+    def __init__(self, model, inputs, op_type, input_full_names):
         self.raw_operator = model
         self.inputs = inputs
+        self.input_full_names = input_full_names
         self.type = op_type
 
 def convert_sklearn_feature_union(scope, operator, container):
     op = operator.raw_operator
-    transform_result_name = [] 
+    transform_result_name = []
+    supported_transformer_list = [PCA, TruncatedSVD]
     for name, transform in op.transformer_list:
-        op_type = sklearn_operator_name_map[type(transform)] 
-        this_operator = Oper(transform, operator.inputs, op_type)
+        if type(transform) not in supported_transformer_list:
+            raise NotImplementedError
+        op_type = sklearn_operator_name_map[type(transform)]
+        this_operator = Oper(transform, operator.inputs, op_type, operator.input_full_names)
         transform_result_name.append(get_converter(op_type)(scope, this_operator, container, flag=True))
 
     container.add_node('Concat', [s for s in transform_result_name],
--- onnxmltools/convert/sklearn/shape_calculators/FeatureUnion.py
+++ onnxmltools/convert/sklearn/shape_calculators/FeatureUnion.py
@@ -4,22 +4,13 @@
 # license information.
 # --------------------------------------------------------------------------
 
-import copy
 from ...common._registration import register_shape_calculator
 from ...common.data_types import Int64TensorType, StringTensorType, FloatTensorType
 from ...common.utils import check_input_and_output_numbers, check_input_and_output_types
 
 
 def calculate_sklearn_feature_union(operator):
-    '''
-    This function just copy the input shape to the output because label encoder only alters input features' values, not
-    their shape.
-    '''
     check_input_and_output_numbers(operator, output_count_range=1)
-    check_input_and_output_types(operator, good_input_types=[Int64TensorType, StringTensorType, FloatTensorType])
-
-    input_shape = copy.deepcopy(operator.inputs[0].type.shape)
-    operator.outputs[0].type = FloatTensorType(copy.deepcopy(input_shape))
 
 
 register_shape_calculator('SklearnFeatureUnion', calculate_sklearn_feature_union)
