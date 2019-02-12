278c342e260a1ea59a383efbd5038e5faa38ad60
xavier dupré <xavier.dupre@gmail.com>
2019-02-12 11:27:14 +0100

    Fixes #234, fixes lightgbm when the root of a tree is a leave

---
+++ tests/lightgbm/test_LightGbmTreeEnsembleConvertersPkl.py
@@ -0,0 +1,30 @@
+# -------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for
+# license information.
+# --------------------------------------------------------------------------
+
+import unittest
+import numpy
+import pickle
+import os
+from onnxmltools import convert_lightgbm
+from onnxmltools.convert.common.data_types import FloatTensorType
+from onnxmltools.utils import dump_data_and_model
+
+
+class TestLightGbmTreeEnsembleModelsPkl(unittest.TestCase):
+
+    def test_root_leave(self):
+        this = os.path.abspath(os.path.dirname(__file__))
+        for name in ["example.pkl"]:
+            with open(os.path.join(this, name), "rb") as f:
+                model = pickle.load(f)
+            X = [[0., 1.], [1., 1.], [2., 0.]]
+            X = numpy.array(X, dtype=numpy.float32)
+            model_onnx = convert_lightgbm(model.steps[1][1], 'pkl1', [('input', FloatTensorType([1, X.shape[1]]))])
+            dump_data_and_model(X, model.steps[1][1], model_onnx, basename="LightGbmPkl1")
+
+
+if __name__ == "__main__":
+    unittest.main()
