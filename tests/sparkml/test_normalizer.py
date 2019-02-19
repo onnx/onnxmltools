5f25bdc7ec25b71c80f5b19a5257e67d0a887fc9
Jeff Saremi <jeffsaremi@hotmail.com>
2019-02-19 14:35:06 -0800

    fixing unit tests for sparkml

--- tests/sparkml/test_normalizer.py
+++ tests/sparkml/test_normalizer.py
@@ -32,6 +32,25 @@ class TestSparkmlNormalizer(SparkMlTestCase):
         data_np = data.toPandas().features.apply(lambda x: pandas.Series(x.toArray())).values.astype(numpy.float32)
         dump_data_and_sparkml_model(data_np, predicted_np, model, model_onnx, basename="SparkmlNormalizer")
 
+    def test_model_normalizer_2(self):
+        import numpy
+        import pandas
+        data = self.spark.createDataFrame([
+          (0, Vectors.dense(1.0, 0.5, -1.0)),
+          (1, Vectors.dense(2.0, 1.0, 1.0)),
+          (2, Vectors.dense(4.0, 10.0, 2.0))
+        ]).toDF("id", "features")
+        model = Normalizer(inputCol='features', outputCol='norm_feature', p=2.0)
+
+        model_onnx = convert_sparkml(model, 'Sparkml Normalizer', [('features', FloatTensorType([1, 3]))])
+        self.assertTrue(model_onnx is not None)
+
+        # run the model
+        predicted = model.transform(data)
+        predicted_np = predicted.toPandas().norm_feature.apply(lambda x: pandas.Series(x.toArray())).values.astype(numpy.float32)
+        data_np = data.toPandas().features.apply(lambda x: pandas.Series(x.toArray())).values.astype(numpy.float32)
+        dump_data_and_sparkml_model(data_np, predicted_np, model, model_onnx, basename="SparkmlNormalizer")
+
 
 if __name__ == "__main__":
     unittest.main()
