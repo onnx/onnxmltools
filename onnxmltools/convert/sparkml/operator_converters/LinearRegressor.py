bd9e6054540dfa280fc1ce4e759b1a482bea2120
Jeff Saremi <jeffsaremi@hotmail.com>
2019-02-26 09:33:27 -0800

    adding LinearRegressor to Spark ML

---
+++ onnxmltools/convert/sparkml/operator_converters/LinearRegressor.py
@@ -0,0 +1,42 @@
+# -------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for
+# license information.
+# --------------------------------------------------------------------------
+import collections
+
+from ...common._registration import register_converter, register_shape_calculator
+from onnxmltools.convert.common.data_types import Int64TensorType, FloatTensorType
+from ...common.utils import check_input_and_output_numbers, check_input_and_output_types
+
+
+def convert_sparkml_linear_regressor(scope, operator, container):
+    op = operator.raw_operator
+    op_type = 'LinearRegressor'
+    attrs = {
+        'name': scope.get_unique_operator_name(op_type),
+        'coefficients': op.coefficients.astype(float),
+        'intercepts': op.intercept.astype(float) if isinstance(op.intercept, collections.Iterable) else [
+                 float(op.intercept)]
+    }
+    container.add_node(op_type, operator.input_full_names, operator.output_full_names, op_domain='ai.onnx.ml', **attrs)
+
+
+register_converter('pyspark.ml.regression.LinearRegressionModel', convert_sparkml_linear_regressor)
+
+
+def calculate_linear_regressor_output_shapes(operator):
+    '''
+    Allowed input/output patterns are
+        1. [N, C] ---> [N, 1]
+
+    This operator produces a scalar prediction for every example in a batch. If the input batch size is N, the output
+    shape may be [N, 1].
+    '''
+    check_input_and_output_numbers(operator, input_count_range=1, output_count_range=1)
+
+    N = operator.inputs[0].type.shape[0]
+    operator.outputs[0].type = FloatTensorType([N, 1])
+
+
+register_shape_calculator('pyspark.ml.regression.LinearRegressionModel', calculate_linear_regressor_output_shapes)
---
+++ tests/sparkml/test_linear_regressor.py
@@ -0,0 +1,59 @@
+"""
+Tests SparkML LinearRegressor converter.
+"""
+import unittest
+import numpy
+from pyspark.ml.linalg import Vectors
+from pyspark.ml.regression import LinearRegression
+
+from onnxmltools import convert_sparkml
+from onnxmltools.convert.common.data_types import FloatTensorType
+from onnxmltools.utils import dump_data_and_sparkml_model
+from sparkml import SparkMlTestCase
+
+
+class TestSparkmlLinearRegression(SparkMlTestCase):
+    def test_model_linear_regression_basic(self):
+        data = self.spark.createDataFrame([
+            (1.0, 2.0, Vectors.dense(1.0)),
+            (0.0, 2.0, Vectors.sparse(1, [], []))
+        ], ["label", "weight", "features"])
+        lr = LinearRegression(maxIter=5, regParam=0.0, solver="normal", weightCol="weight")
+        model = lr.fit(data)
+        # the name of the input is 'features'
+        C = model.numFeatures
+        model_onnx = convert_sparkml(model, 'sparkml LinearRegressorBasic', [('features', FloatTensorType([1, C]))])
+        self.assertTrue(model_onnx is not None)
+        # run the model
+        import pandas
+        predicted = model.transform(data)
+        data_np = data.toPandas().features.apply(lambda x: pandas.Series(x.toArray())).values.astype(numpy.float32)
+        expected = [ predicted.toPandas().prediction.values.astype(numpy.float32) ]
+        dump_data_and_sparkml_model(data_np, expected, model, model_onnx,
+                                    basename="SparkmlLinearRegressor_Basic")
+
+    def test_model_linear_regression(self):
+        import inspect
+        import os
+        this_script_dir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+        input_path = os.path.join(this_script_dir, "data", "sample_linear_regression_data.txt")
+        data = self.spark.read.format("libsvm").load(input_path)
+
+        lr = LinearRegression(maxIter=10, regParam=0.3, elasticNetParam=0.8)
+        model = lr.fit(data)
+        # the name of the input is 'features'
+        C = model.numFeatures
+        model_onnx = convert_sparkml(model, 'sparkml LinearRegressor', [('features', FloatTensorType([1, C]))])
+        self.assertTrue(model_onnx is not None)
+        # run the model
+        import pandas
+        predicted = model.transform(data)
+        data_np = data.toPandas().features.apply(lambda x: pandas.Series(x.toArray())).values.astype(numpy.float32)
+        expected = [ predicted.toPandas().prediction.values.astype(numpy.float32) ]
+        dump_data_and_sparkml_model(data_np, expected, model, model_onnx,
+                                    basename="SparkmlLinearRegressor")
+
+
+
+if __name__ == "__main__":
+    unittest.main()
