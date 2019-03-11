a0cae060b1743b17e7a428b5c6dfb86f1785479d
Jeff Saremi <jeffsaremi@hotmail.com>
2019-03-11 12:10:25 -0700

    fixing the testcase for DecisionTree; DecisionTree conversion is complete

--- tests/sparkml/test_tree_ensemble_classifier.py
+++ tests/sparkml/test_tree_ensemble_classifier.py
@@ -16,45 +16,44 @@ from pyspark.ml.feature import StringIndexer, VectorIndexer
 
 
 class TestSparkmTreeEnsembleClassifier(SparkMlTestCase):
-    # def test_model_tree_ensemble_classifier(self):
-    #     import os
-    #     this_script_dir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
-    #     input_path = os.path.join(this_script_dir, "data", "sample_libsvm_data.txt")
-    #     original_data = self.spark.read.format("libsvm").load(input_path)
-    #     #
-    #     # truncate the features
-    #     #
-    #     feature_count = 5
-    #     self.spark.udf.register("truncateFeatures",
-    #                             lambda x: SparseVector(feature_count, range(0,feature_count), x.toArray()[125:130]),
-    #                             VectorUDT())
-    #     data = original_data.selectExpr("cast(label as string) as label", "truncateFeatures(features) as features")
-    #     label_indexer = StringIndexer(inputCol="label", outputCol="indexedLabel")
-    #     feature_indexer = VectorIndexer(inputCol="features", outputCol="indexedFeatures",
-    #                                     maxCategories=100, handleInvalid='keep')
-    #     (trainingData, testData) = data.randomSplit([0.7, 0.3])
-    #
-    #     dt = DecisionTreeClassifier(labelCol="indexedLabel", featuresCol="indexedFeatures")
-    #     pipeline = Pipeline(stages=[label_indexer, feature_indexer, dt])
-    #     model = pipeline.fit(trainingData)
-    #     model_onnx = convert_sparkml(model, 'Sparkml Decision Tree Pipeline', [
-    #         ('label', StringTensorType([1, 1])),
-    #         ('features', FloatTensorType([1, feature_count]))
-    #     ], spark_session=self.spark)
-    #     self.assertTrue(model_onnx is not None)
-    #     # run the model
-    #     predicted = model.transform(data)
-    #     data_np = {
-    #         'label': data.toPandas().label.values,
-    #         'features': data.toPandas().features.apply(lambda x: pandas.Series(x.toArray())).values.astype(numpy.float32)
-    #     }
-    #     expected = [
-    #         predicted.toPandas().indexedLabel.values.astype(numpy.int64),
-    #         predicted.toPandas().prediction.values.astype(numpy.float32),
-    #         predicted.toPandas().probability.apply(lambda x: pandas.Series(x.toArray())).values.astype(numpy.float32)
-    #     ]
-    #     dump_data_and_sparkml_model(data_np, expected, model, model_onnx,
-    #                                 basename="SparkmlDecisionTreePipeline")
+    def test_tree_pipeline(self):
+        import os
+        this_script_dir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+        input_path = os.path.join(this_script_dir, "data", "sample_libsvm_data.txt")
+        original_data = self.spark.read.format("libsvm").load(input_path)
+        #
+        # truncate the features
+        #
+        feature_count = 5
+        self.spark.udf.register("truncateFeatures",
+                                lambda x: SparseVector(feature_count, range(0,feature_count), x.toArray()[125:130]),
+                                VectorUDT())
+        data = original_data.selectExpr("cast(label as string) as label", "truncateFeatures(features) as features")
+        label_indexer = StringIndexer(inputCol="label", outputCol="indexedLabel")
+        feature_indexer = VectorIndexer(inputCol="features", outputCol="indexedFeatures",
+                                        maxCategories=10, handleInvalid='keep')
+
+        dt = DecisionTreeClassifier(labelCol="indexedLabel", featuresCol="indexedFeatures")
+        pipeline = Pipeline(stages=[label_indexer, feature_indexer, dt])
+        model = pipeline.fit(data)
+        model_onnx = convert_sparkml(model, 'Sparkml Decision Tree Pipeline', [
+            ('label', StringTensorType([1, 1])),
+            ('features', FloatTensorType([1, feature_count]))
+        ], spark_session=self.spark)
+        self.assertTrue(model_onnx is not None)
+        # run the model
+        predicted = model.transform(data)
+        data_np = {
+            'label': data.toPandas().label.values,
+            'features': data.toPandas().features.apply(lambda x: pandas.Series(x.toArray())).values.astype(numpy.float32)
+        }
+        expected = [
+            predicted.toPandas().indexedLabel.values.astype(numpy.int64),
+            predicted.toPandas().prediction.values.astype(numpy.float32),
+            predicted.toPandas().probability.apply(lambda x: pandas.Series(x.toArray())).values.astype(numpy.float32)
+        ]
+        dump_data_and_sparkml_model(data_np, expected, model, model_onnx,
+                                    basename="SparkmlDecisionTreePipeline")
 
     def test_tree_one_class_classification(self):
         features = [[0., 1.], [1., 1.], [2., 0.]]
