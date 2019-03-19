2d19768f540cc19fdae4d4cae21d083f2384b86e
Vinitra Swamy <vinitras@gmail.com>
2019-03-19 16:34:08 -0700

    Resolve alpha/beta default values, use ValueError check for both cases

--- onnxmltools/convert/common/_apply_operation.py
+++ onnxmltools/convert/common/_apply_operation.py
@@ -421,12 +421,16 @@ def apply_parametric_softplus(scope, input_name, output_name, container, operato
         apply_mul(scope, [fName, aName], output_name, container)
 
 
-def apply_scaled_tanh(scope, input_name, output_name, container, operator_name=None, alpha=[1.0], beta=[1.0]):
+def apply_scaled_tanh(scope, input_name, output_name, container, operator_name=None, alpha=None, beta=None):
+    if alpha == None:
+        alpha = [1.0]
+    if beta == None:
+        beta = [1.0]
+    if len(alpha) != 1 or len(beta) != 1:
+        raise ValueError('alpha and beta must be 1-element lists')
 
     name = _create_name_or_use_existing_one(scope, 'ScaledTanh', operator_name)
     if container.target_opset < 9:
-        if len(alpha) != 1 or len(beta) != 1:
-            raise ValueError('alpha and beta must be 1-element lists')
         attrs = {'name': name, 'alpha': alpha[0], 'beta': beta[0]}
         container.add_node('ScaledTanh', input_name, output_name, **attrs)
     else:
