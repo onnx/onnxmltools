c399411f4e312171ea22e1edda9b2d869f3704cf
Jeff Saremi <jeffsaremi@hotmail.com>
2019-04-17 11:49:40 -0700

    copyright headers

--- onnxmltools/convert/sparkml/ops_input_output.py
+++ onnxmltools/convert/sparkml/ops_input_output.py
@@ -1,3 +1,8 @@
+# -------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for
+# license information.
+# --------------------------------------------------------------------------
 '''
 Mapping and utilities for the names of Params(propeties) that various Spark ML models
 have for their input and output columns
--- onnxmltools/convert/sparkml/utils.py
+++ onnxmltools/convert/sparkml/utils.py
@@ -1,3 +1,8 @@
+# -------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for
+# license information.
+# --------------------------------------------------------------------------
 '''
 Utility functions for Spark ML to Onnx conversion intended for the end user mainly
 '''
