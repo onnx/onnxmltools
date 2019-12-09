78ab0f6feff0f6a74bcfa5f510148505b5564170
xavier dupré <xavier.dupre@gmail.com>
2019-12-09 15:40:27 +0100

    delay import

--- tests/coreml/test_cml_ImputerConverter.py
+++ tests/coreml/test_cml_ImputerConverter.py
@@ -7,7 +7,7 @@ try:
     from sklearn.impute import SimpleImputer as Imputer
 except ImportError:
     from sklearn.preprocessing import Imputer
-import coremltools
+import sklearn.preprocessing
 from onnxmltools.convert.coreml.convert import convert
 from onnxmltools.utils import dump_data_and_model
 
@@ -15,9 +15,14 @@ from onnxmltools.utils import dump_data_and_model
 class TestCoreMLImputerConverter(unittest.TestCase):
 
     def test_imputer(self):
+        if not hasattr(sklearn.preprocessing, 'Imputer'):
+            # coremltools 3.1 does not work with scikit-learn 0.22
+            setattr(sklearn.preprocessing, 'Imputer', Imputer)
+        
         model = Imputer(missing_values='NaN', strategy='mean', axis=0)
         data = [[1, 2], [np.nan, 3], [7, 6]]
         model.fit(data)
+        import coremltools  # noqa
         model_coreml = coremltools.converters.sklearn.convert(model)
         model_onnx = convert(model_coreml.get_spec())
         self.assertTrue(model_onnx is not None)
