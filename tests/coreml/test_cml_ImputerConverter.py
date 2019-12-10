d924d56f0b5c56e64a241e81089e393fd86989fa
xavier dupré <xavier.dupre@gmail.com>
2019-12-10 19:49:33 +0100

    Update test_cml_ImputerConverter.py

--- tests/coreml/test_cml_ImputerConverter.py
+++ tests/coreml/test_cml_ImputerConverter.py
@@ -27,7 +27,12 @@ class TestCoreMLImputerConverter(unittest.TestCase):
         model.fit(data)
         from onnxmltools.convert.coreml.convert import convert
         import coremltools  # noqa
-        model_coreml = coremltools.converters.sklearn.convert(model)
+        try:
+            model_coreml = coremltools.converters.sklearn.convert(model)
+        except ValueError as e:
+            if 'not supported' in str(e):
+                # Python 2.7 + scikit-learn 0.22
+                return
         model_onnx = convert(model_coreml.get_spec())
         self.assertTrue(model_onnx is not None)
         dump_data_and_model(np.array(data, dtype=np.float32),
