dfd089be7b5ae4e9b9ede0f6a629be92d035f658
Wenbing Li <wenli@microsoft.com>
2020-01-28 16:38:51 -0800

    add tf2onnx wrapper test. (#367)
    * add tf2onnx wrapper test.
    
    * install tf 1.15.0
    
    * install tf2onnx

--- tests/utils/test_utils.py
+++ tests/utils/test_utils.py
@@ -2,7 +2,9 @@
 Tests utilities.
 """
 import os
+import six
 import unittest
+import onnxmltools
 from onnxmltools.utils import load_model, save_model
 from onnxmltools.utils import set_model_version, set_model_domain, set_model_doc_string
 
@@ -51,6 +53,22 @@ class TestUtils(unittest.TestCase):
         self.assertRaises(ValueError, set_model_doc_string, onnx_model.doc_string, "sample")
         set_model_doc_string(onnx_model, "", True)
         self.assertEqual(onnx_model.doc_string, "")
+
+
+@unittest.skipIf(six.PY2, "Keras and Tensorflow converter not support python 2.x")
+class TestWrapper(unittest.TestCase):
+
+    def test_keras_with_tf2onnx(self):
+        import keras2onnx
+        from keras2onnx.proto import keras
+        from keras2onnx.proto.tfcompat import is_tf2
+        if not is_tf2:  # tf2onnx is not available for tensorflow 2.0 yet.
+            model = keras.Sequential()
+            model.add(keras.layers.Dense(units=4, input_shape=(10,), activation='relu'))
+            model.compile(loss='binary_crossentropy', optimizer='Adam', metrics=['binary_accuracy'])
+            graph_def = keras2onnx.export_tf_frozen_graph(model)
+            onnx_model = onnxmltools.convert_tensorflow(graph_def, **keras2onnx.build_io_names_tf2onnx(model))
+            self.assertTrue(len(onnx_model.graph.node) > 0)
 
 
 if __name__ == "__main__":
