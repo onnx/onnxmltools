9025eff9e8e1284602f4dc9963ccc7f673c41595
xavier dupré <xavier.dupre@gmail.com>
2020-03-04 13:18:12 +0100

    more robust CI, + new test on xgboost

--- tests/xgboost/test_xgboost_unpickle_06.py
+++ tests/xgboost/test_xgboost_unpickle_06.py
@@ -3,15 +3,19 @@ Tests scikit-linear converter.
 """
 import sys
 import os
-from onnxmltools.convert.xgboost import convert as convert_xgboost
+from distutils.version import StrictVersion
 import unittest
 import pickle
+import xgboost
+from onnxmltools.convert.xgboost import convert as convert_xgboost
 from onnxmltools.convert.common.data_types import FloatTensorType
 
 
 class TestXGBoostUnpickle06(unittest.TestCase):
 
     @unittest.skipIf(sys.version_info[0] == 2, reason="xgboost converted not tested on python 2")
+    @unittest.skipIf(StrictVersion(xgboost.__version__) >= StrictVersion('1.0'),
+                     reason="compatibility break with pickle in 1.0")
     def test_xgboost_unpickle_06(self):
         # Unpickle a model trained with an old version of xgboost.
         this = os.path.dirname(__file__)
