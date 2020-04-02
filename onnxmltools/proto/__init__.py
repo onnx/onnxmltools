8208d327556faacd4afdcec90d27e32ca59052d0
Wenbing Li <wenli@microsoft.com>
2020-04-01 17:13:29 -0700

    set the default versoin of the converters

--- onnxmltools/proto/__init__.py
+++ onnxmltools/proto/__init__.py
@@ -8,7 +8,9 @@ def _check_onnx_version():
     import pkg_resources
     min_required_version = pkg_resources.parse_version('1.0.1')
     current_version = pkg_resources.get_distribution('onnx').parsed_version
-    assert current_version >= min_required_version , 'ONNXMLTools requires ONNX version 1.0.1 or a newer one'
+    assert current_version >= min_required_version, 'ONNXMLTools requires ONNX version 1.0.1 or a newer one'
+
+
 _check_onnx_version()
 
 # Rather than using ONNX protobuf definition throughout our codebase, we import ONNX protobuf definition here so that
@@ -21,6 +23,8 @@ from onnx import helper
 from onnx import mapping
 from onnx.onnx_pb import TensorProto
 from onnx.helper import split_complex_to_pairs
+
+
 def _make_tensor_fixed(name, data_type, dims, vals, raw=False):
     '''
     Make a TensorProto with specified arguments.  If raw is False, this
@@ -51,4 +55,10 @@ helper.make_tensor = _make_tensor_fixed
 
 
 def get_opset_number_from_onnx():
-    return onnx.defs.onnx_opset_version()
+    default_max_opset = 11
+    try:
+        from onnxconverter_common.topology import DEFAULT_OPSET_NUMBER
+        default_max_opset = DEFAULT_OPSET_NUMBER
+    except:
+        pass
+    return min(default_max_opset, onnx.defs.onnx_opset_version())
