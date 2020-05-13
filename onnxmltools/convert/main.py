1bb4d0756a0e7fc4e6e864799b5b1c88a08b06cc
students <aavolodkevich@beleriand.velkerr.ru>
2020-05-13 20:25:47 +0300

    convert catboost

--- onnxmltools/convert/main.py
+++ onnxmltools/convert/main.py
@@ -69,7 +69,6 @@ def convert_catboost(model, name=None, initial_types=None, doc_string='', target
     return onnx_model
 
 
-
 def convert_lightgbm(model, name=None, initial_types=None, doc_string='', target_opset=None,
                      targeted_onnx=onnx.__version__, custom_conversion_functions=None, custom_shape_calculators=None):
     if not utils.lightgbm_installed():
