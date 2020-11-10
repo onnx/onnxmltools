d58258144721503873e0f28d7e76e57c24ce644a
ibadr <ibadr@users.noreply.github.com>
2020-11-10 22:38:00 +0200

    Extend CoreML: ReshapeStatic/LoadConstantND
    Signed-off-by: Islam <ibadreldin@icloud.com>

---
+++ onnxmltools/convert/coreml/operator_converters/neural_network/LoadConstantND.py
@@ -0,0 +1,21 @@
+# -------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for
+# license information.
+# --------------------------------------------------------------------------
+
+from .....proto import helper
+from .....proto import onnx_proto
+from ....common._registration import register_converter
+from ....common._apply_operation import apply_constant
+
+def convert_load_constant_nd(scope, operator, container):
+    params = operator.raw_operator.loadConstantND
+    constant_name = scope.get_unique_variable_name('constant')
+    constant = helper.make_tensor(constant_name, onnx_proto.TensorProto.FLOAT,
+                                  params.shape, params.data.floatValue)
+
+    apply_constant(scope, operator.output_full_names, container,
+                   operator_name=operator.full_name, value=constant)
+
+register_converter('loadConstantND', convert_load_constant_nd)
