6a0d136d132f4d9ba0493835f9930a1734ca1d2b
xavier dupré <xavier.dupre@gmail.com>
2021-09-20 17:42:38 +0200

    update CI
    Signed-off-by: xavier dupré <xavier.dupre@gmail.com>

--- tests/h2o/test_h2o_converters.py
+++ tests/h2o/test_h2o_converters.py
@@ -151,7 +151,7 @@ class TestH2OModels(unittest.TestCase):
             h2o.remove_all()
         except H2OConnectionError as e:
             print(e)
-            sys.exit(1)
+            sys.exit(0)
 
     def test_h2o_unsupported_algo(self):
         gbm = H2ORandomForestEstimator(ntrees=7, max_depth=5)
