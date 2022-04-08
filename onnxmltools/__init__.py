aeae6e8f4518d8da02a901ab6d90318e5cfe1705
xiaowuhu <xiaowuhu@microsoft.com>
2022-04-08 16:56:38 +0800

    update author info
    Signed-off-by: xiaowuhu <xiaowuhu@microsoft.com>

--- onnxmltools/__init__.py
+++ onnxmltools/__init__.py
@@ -6,7 +6,7 @@ This framework converts any machine learned model into onnx format
 which is a common language to describe any machine learned model.
 """
 __version__ = "1.11.0"
-__author__ = "Microsoft"
+__author__ = "ONNX"
 __producer__ = "OnnxMLTools"
 __producer_version__ = __version__
 __domain__ = "onnxml"
