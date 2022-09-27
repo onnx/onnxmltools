b4b3f4df543d93c2df57678a906d3333699b3e36
xadupre <xadupre@microsoft.com>
2022-09-27 15:04:53 +0200

    convert range attribute into list
    Signed-off-by: xadupre <xadupre@microsoft.com>

--- onnxmltools/convert/sparkml/operator_converters/decision_tree_classifier.py
+++ onnxmltools/convert/sparkml/operator_converters/decision_tree_classifier.py
@@ -21,6 +21,22 @@ def convert_decision_tree_classifier(scope, operator, container):
     tree = sparkml_tree_dataset_to_sklearn(tree_df, is_classifier=True)
     add_tree_to_attribute_pairs(attrs, True, tree, 0, 1., 0, True)
 
+    # Spark may store attributes as range and not necessary list.
+    # ONNX does not support this type of attribute value.
+    update = {}
+    wrong_types = []
+    for k, v in attrs.items():
+        if isinstance(v, (str, list)):
+            continue
+        if isinstance(v, range):
+            update[k] = list(v)
+            continue
+        wrong_types.append(f"Unexpected type {type(v)} for attribute {k!r}.")
+    if len(wrong_types) > 0:
+        raise TypeError("Unexpected type for one or several attributes:\n" + "\n".join(wrong_types))
+    if update:
+        attrs.update(update)
+
     container.add_node(op_type, operator.input_full_names, [operator.outputs[0].full_name,
                        operator.outputs[1].full_name], op_domain='ai.onnx.ml', **attrs)
 
