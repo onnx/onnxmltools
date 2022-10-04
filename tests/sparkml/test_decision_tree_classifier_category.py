f9b439e6bddf9be6e6366bb2a9687c6d7add487a
xadupre <xadupre@microsoft.com>
2022-10-04 21:16:25 +0200

    still working
    Signed-off-by: xadupre <xadupre@microsoft.com>

--- tests/sparkml/test_decision_tree_classifier_category.py
+++ tests/sparkml/test_decision_tree_classifier_category.py
@@ -12,6 +12,7 @@ from sklearn.datasets import dump_svmlight_file
 from pyspark.ml import Pipeline
 from pyspark.ml.classification import DecisionTreeClassifier
 from pyspark.ml.linalg import VectorUDT, SparseVector, Vectors
+from pyspark.ml.feature import VectorAssembler
 from onnx.defs import onnx_opset_version
 from onnxconverter_common.onnx_ex import DEFAULT_OPSET_NUMBER
 from onnxmltools import convert_sparkml
@@ -34,27 +35,33 @@ class TestSparkmDecisionTreeClassifierBig(SparkMlTestCase):
     @unittest.skipIf(pv.Version(onnx.__version__) <= pv.Version('1.3'), 'Need Greater Opset 9')
     def test_tree_pipeline_category(self):
 
-        features = numpy.random.randint(0, high=100, size=(10000, 10)) % 100
+        features = numpy.random.randint(0, high=100, size=(10000, 50)) % 100
         features[:, :features.shape[1]-1] = features[:, :features.shape[1]-1] % 10
         features[:, :] *= numpy.random.randint(0, high=2, size=features.shape) % 2
-        labels = features[:, -1] % 2
+        labels = (features.sum(axis=1) + features[:, -1] * 4)
+        labels //= (labels.max() // 2 + 1)
         n_features = features.shape[1]
-        features = features.tolist()
-        labels = labels.tolist()
-        #input_path = os.path.join(this_script_dir, "sample_libsvm_catdata.svm")
-        #dump_svmlight_file(mat, label, input_path)
+        df = pandas.DataFrame(features)
+        features_names = [f"c{i}" for i in range(df.shape[1])]
+        df.columns = features_names
+        df["label"] = labels
+        print(df.head())
+        input_path = os.path.join(".", "sample_catdata.csv")
+        df.to_csv(input_path, index=False)
 
-        #this_script_dir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
-        #original_data = self.spark.read.format("libsvm").load(input_path)
-        dd = [(labels[i], Vectors.dense(features[i])) for i in range(len(labels))]
-        data = self.spark.createDataFrame(self.spark.sparkContext.parallelize(dd), schema=["label", "features"])
-        dt = DecisionTreeClassifier(labelCol="label", featuresCol="features", maxDepth=3)
-        model = dt.fit(data)
+        data = self.spark.read.csv(input_path, header=True, inferSchema=True)
+        print(data.printSchema())
+        va = VectorAssembler(inputCols=features_names, outputCol='features')
+        va_df = va.transform(data)
+        va_df = va_df.select(['features', 'label'])
+
+        dt = DecisionTreeClassifier(labelCol="label", featuresCol='features', maxDepth=15)
+        model = dt.fit(va_df)
         model_onnx = convert_sparkml(model, 'Sparkml Decision Tree Binary Class', [
             ('features', FloatTensorType([None, n_features]))
         ], spark_session=self.spark, target_opset=TARGET_OPSET)
-        data_np = data.toPandas().features.apply(lambda x: pandas.Series(x.toArray())).values.astype(numpy.float32)
-        predicted = model.transform(data)
+        data_np = va_df.toPandas().features.apply(lambda x: pandas.Series(x.toArray())).values.astype(numpy.float32)
+        predicted = model.transform(va_df)
         expected = [
             predicted.toPandas().prediction.values.astype(numpy.float32),
             predicted.toPandas().probability.apply(lambda x: pandas.Series(x.toArray())).values.astype(numpy.float32)
