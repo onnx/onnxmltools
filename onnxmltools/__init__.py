a08bcbc124f9d350d071e1eb81dac1ee1cac3534
xadupre <xadupre@microsoft.com>
2022-10-19 18:20:20 +0200

    change version number
    Signed-off-by: xadupre <xadupre@microsoft.com>

--- onnxmltools/__init__.py
+++ onnxmltools/__init__.py
@@ -5,7 +5,7 @@ Main entry point to onnxmltools.
 This framework converts any machine learned model into onnx format
 which is a common language to describe any machine learned model.
 """
-__version__ = "1.11.110"
+__version__ = "1.11.2"
 __author__ = "ONNX"
 __producer__ = "OnnxMLTools"
 __producer_version__ = __version__
