8a0a6831426191f24bef7f834d09ab53748c5035
Xavier Dupré <xadupre@users.noreply.github.com>
2023-08-01 10:31:16 +0200

    Check if base_score is available and it is a string type convert it to float (#637)
    Signed-off-by: Donald Tolley <tolleybot@gmail.com>
    Signed-off-by: Xavier Dupre <xadupre@microsoft.com>
    Co-authored-by: Donald Tolley <tolleybot@gmail.com>

--- onnxmltools/convert/xgboost/_parse.py
+++ onnxmltools/convert/xgboost/_parse.py
@@ -105,6 +105,8 @@ def _get_attributes(booster):
 
     if "base_score" not in kwargs:
         kwargs["base_score"] = 0.5
+    elif isinstance(kwargs["base_score"], str):
+        kwargs["base_score"] = float(kwargs["base_score"])
     return kwargs
 
 
