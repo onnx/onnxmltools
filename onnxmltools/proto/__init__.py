908630759f64bcde0833e75254f269b68954e257
Justin Chu <justinchuby@users.noreply.github.com>
2025-05-14 13:39:42 -0700

    More
    Signed-off-by: Justin Chu <justinchuby@users.noreply.github.com>

--- onnxmltools/proto/__init__.py
+++ onnxmltools/proto/__init__.py
@@ -4,10 +4,7 @@
 # we import ONNX protobuf definition here so that
 # we can conduct quick fixes by overwriting ONNX functions without
 # changing any lines elsewhere.
-from onnx import onnx_pb as onnx_proto  # noqa
+from onnx import onnx_pb as onnx_proto
 from onnx import helper
 
-# Overwrite the make_tensor defined in onnx.helper because of
-# a bug (string tensor get assigned twice)
-from onnx import mapping
 from onnx.onnx_pb import TensorProto
