d994a488b0fa894ae09a46e696efd3a70289928f
Andreas Fehlner <fehlner@arcor.de>
2026-01-07 14:03:29 +0100

    migrate to pyproject.toml; fix dependency failures  (#750)
    * Update requirements-dev.txt
    
    Signed-off-by: Andreas Fehlner <fehlner@arcor.de>
    
    * Update ci.yml
    
    Signed-off-by: Andreas Fehlner <fehlner@arcor.de>
    
    * Update ci.yml
    
    Signed-off-by: Andreas Fehlner <fehlner@arcor.de>
    
    * Update ci.yml
    
    Signed-off-by: Andreas Fehlner <fehlner@arcor.de>
    
    * Update ci.yml
    
    Signed-off-by: Andreas Fehlner <fehlner@arcor.de>
    
    * Update pyproject.toml
    
    Signed-off-by: Andreas Fehlner <fehlner@arcor.de>
    
    * Update setup.py
    
    Signed-off-by: Andreas Fehlner <fehlner@arcor.de>
    
    * Delete setup.py
    
    Signed-off-by: Andreas Fehlner <fehlner@arcor.de>
    
    * Update pyproject.toml
    
    Signed-off-by: Andreas Fehlner <fehlner@arcor.de>
    
    * Update ci.yml
    
    Signed-off-by: Andreas Fehlner <fehlner@arcor.de>
    
    * Update and rename black-ruff.yml to ruff.yml
    
    Signed-off-by: Andreas Fehlner <fehlner@arcor.de>
    
    * Update setup.cfg
    
    Signed-off-by: Andreas Fehlner <fehlner@arcor.de>
    
    * Update ci.yml
    
    Signed-off-by: Andreas Fehlner <fehlner@arcor.de>
    
    * Delete setup.cfg
    
    Signed-off-by: Andreas Fehlner <fehlner@arcor.de>
    
    * Update __init__.py
    
    Signed-off-by: Andreas Fehlner <fehlner@arcor.de>
    
    ---------
    
    Signed-off-by: Andreas Fehlner <fehlner@arcor.de>

--- onnxmltools/__init__.py
+++ onnxmltools/__init__.py
@@ -5,13 +5,12 @@ Main entry point to onnxmltools.
 This framework converts any machine learned model into onnx format
 which is a common language to describe any machine learned model.
 """
-__version__ = "1.14.0"
+__version__ = "1.15.0rc1"
 __author__ = "ONNX"
 __producer__ = "OnnxMLTools"
 __producer_version__ = __version__
 __domain__ = "onnxml"
 __model_version__ = 0
-
 
 from .convert import convert_coreml
 from .convert import convert_keras
