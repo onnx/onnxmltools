012cc6a1652d407eba396bd1e6e3a04f8c936e57
Xavier Dupré <xadupre@microsoft.com>
2026-01-30 12:10:34 +0100

    fix unit test checking zipmap
    Signed-off-by: Xavier Dupré <xadupre@microsoft.com>

--- tests/lightgbm/test_LightGbmTreeEnsembleConverters.py
+++ tests/lightgbm/test_LightGbmTreeEnsembleConverters.py
@@ -39,8 +39,8 @@ class TestLightGbmTreeEnsembleModels(unittest.TestCase):
             "dummy",
             input_types=[("X", FloatTensorType([None, X.shape[1]]))],
             target_opset=TARGET_OPSET,
-        )
-        assert "zipmap" in str(onx).lower()
+        )[0]
+        assert "ZipMap" in [n.op_type for n in onx.graph.node]
 
     def test_lightgbm_classifier_nozipmap(self):
         X = [[0, 1], [1, 1], [2, 0], [1, 2], [1, 5], [6, 2]]
